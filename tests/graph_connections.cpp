d935a820462a51444cc22e83c0b2e08658e599bc
Martin Prazak <martin.prazak@gmail.com>
2017-01-19 22:55:51 +0100

    Added simple connection callback tests

--- tests/graph_connections.cpp
+++ tests/graph_connections.cpp
@@ -19,10 +19,17 @@ namespace {
 
 		return connections.empty() && result;
 	}
+
+	unsigned s_connectionCount = 0;
 }
 
 BOOST_AUTO_TEST_CASE(graph_connections) {
 	Graph g;
+
+	//////
+	// connect callbacks
+	g.onConnect([&](Node::Port&, Node::Port&) { ++s_connectionCount; });
+	g.onDisconnect([&](Node::Port&, Node::Port&) { --s_connectionCount; });
 
 	// instantiate nodes from arithmetic.cpp
 	const Metadata& addition = additionNode();
@@ -37,6 +44,8 @@ BOOST_AUTO_TEST_CASE(graph_connections) {
 	BOOST_REQUIRE_EQUAL(g.nodes().size(), 4u);
 	BOOST_REQUIRE_EQUAL(g.connections().size(), 0u);
 	BOOST_REQUIRE(g.connections().begin() == g.connections().end());
+
+	BOOST_CHECK_EQUAL(s_connectionCount, 0u);
 
 	//////////
 	// adding
@@ -55,6 +64,8 @@ BOOST_AUTO_TEST_CASE(graph_connections) {
 	BOOST_CHECK_EQUAL(&(g.connections().begin()->first), &add1.port(2));
 	BOOST_CHECK_EQUAL(&(g.connections().begin()->second), &mult1.port(1));
 
+	BOOST_CHECK_EQUAL(s_connectionCount, 1u);
+
 	// add another connection
 	BOOST_CHECK_NO_THROW(add1.port(2).connect(mult2.port(1)));
 
@@ -66,6 +77,8 @@ BOOST_AUTO_TEST_CASE(graph_connections) {
 		BOOST_REQUIRE(it == g.connections().end());
 	}
 	BOOST_REQUIRE(checkConnections(g, {{&add1.port(2), &mult1.port(1)}, {&add1.port(2), &mult2.port(1)}}));
+
+	BOOST_CHECK_EQUAL(s_connectionCount, 2u);
 
 	// add another two connections to complete a graph
 	BOOST_CHECK_NO_THROW(mult1.port(2).connect(add2.port(0)));
@@ -85,10 +98,14 @@ BOOST_AUTO_TEST_CASE(graph_connections) {
 		{&mult2.port(2), &add2.port(1)}
 	}));
 
+	BOOST_CHECK_EQUAL(s_connectionCount, 4u);
+
 	// try to add invalid connections
 	BOOST_CHECK_THROW(add1.port(1).connect(mult1.port(1)), std::runtime_error);
 	BOOST_CHECK_THROW(add1.port(2).connect(mult1.port(2)), std::runtime_error);
 	BOOST_CHECK_THROW(add1.port(1).connect(mult1.port(2)), std::runtime_error);
+
+	BOOST_CHECK_EQUAL(s_connectionCount, 4u);
 
 	/////////
 	// removing
@@ -103,13 +120,19 @@ BOOST_AUTO_TEST_CASE(graph_connections) {
 		{&mult2.port(2), &add2.port(1)}
 	}));
 
+	BOOST_CHECK_EQUAL(s_connectionCount, 3u);
+
 	// try to remove the same connection again
 	BOOST_CHECK_THROW(add1.port(2).disconnect(mult1.port(1)), std::runtime_error);
+
+	BOOST_CHECK_EQUAL(s_connectionCount, 3u);
 
 	// try to remove invalid connections
 	BOOST_CHECK_THROW(add1.port(1).disconnect(mult1.port(1)), std::runtime_error);
 	BOOST_CHECK_THROW(add1.port(2).disconnect(mult1.port(2)), std::runtime_error);
 	BOOST_CHECK_THROW(add1.port(1).disconnect(mult1.port(2)), std::runtime_error);
+
+	BOOST_CHECK_EQUAL(s_connectionCount, 3u);
 
 	// remove mult2 node, which should also remove relevant connections
 	auto it = std::find_if(g.nodes().begin(), g.nodes().end(), [&](const Node& n) {
@@ -124,9 +147,13 @@ BOOST_AUTO_TEST_CASE(graph_connections) {
 		{&mult1.port(2), &add2.port(0)}
 	}));
 
+	BOOST_CHECK_EQUAL(s_connectionCount, 1u);
+
 	// and clear() shold remove the rest
 	g.clear();
 	BOOST_REQUIRE_EQUAL(g.connections().size(), 0u);
 	BOOST_REQUIRE(g.connections().empty());
 	BOOST_REQUIRE(g.nodes().empty());
+
+	BOOST_CHECK_EQUAL(s_connectionCount, 0u);
 }
