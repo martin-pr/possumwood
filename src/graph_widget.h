8218a683fb518d85df2c6b0dc9e6f784ff5e7df9
Martin Prazak <martin.prazak@gmail.com>
2017-01-31 23:30:20 +0100

    Removed unnecessary context menu and keypress callbacks and changed sandbox to use standard Qt mechanisms to achieve the same effect

--- src/graph_widget.h
+++ src/graph_widget.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <functional>
-
 #include <QGraphicsView>
 
 #include "node.h"
@@ -18,22 +16,12 @@ class GraphWidget : public QGraphicsView {
 
 		GraphScene& scene();
 
-		// QGraphView is not a QWidget - cannot use Qt's signals/slots.
-		// Whatever - let's do it via functors.
-		void setContextMenuCallback(std::function<void(QPoint)> fn);
-		void setKeyPressCallback(std::function<void(const QKeyEvent&)> fn);
-
 	protected:
 		virtual void resizeEvent(QResizeEvent* event) override;
-		virtual void mousePressEvent(QMouseEvent* event) override;
 		virtual void mouseMoveEvent(QMouseEvent* event) override;
-		virtual void keyPressEvent(QKeyEvent* event) override;
 
 	private:
 		GraphScene m_scene;
-
-		std::function<void(QPoint)> m_contextMenuCallback;
-		std::function<void(const QKeyEvent&)> m_keyPressCallback;
 };
 
 }
