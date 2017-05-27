29bcc4c64b4e7d2acf4507c824a424b4d57ecf65
Martin Prazak <martin.prazak@gmail.com>
2017-05-27 01:55:28 +0100

    Simplified dependency_graph::Datablock class

--- src/libs/dependency_graph/datablock.h
+++ src/libs/dependency_graph/datablock.h
@@ -8,12 +8,6 @@ namespace dependency_graph {
 class Metadata;
 class Node;
 
-template<typename T>
-class InAttr;
-
-template<typename T>
-class OutAttr;
-
 /// A data storage class used by Node implementation.
 /// Each data value is strongly typed, and stored as base class pointer.
 class Datablock {
@@ -38,13 +32,6 @@ class Datablock {
 		Datablock(const Metadata& meta);
 
 		template<typename T>
-		const T& get(const InAttr<T>& attr) const;
-
-		template<typename T>
-		void set(const OutAttr<T>& attr, const T& value);
-
-	protected:
-		template<typename T>
 		const T& get(size_t index) const;
 
 		template<typename T>
@@ -55,8 +42,6 @@ class Datablock {
 
 	private:
 		std::vector<std::unique_ptr<BaseData>> m_data;
-
-		friend class Node;
 };
 
 }
