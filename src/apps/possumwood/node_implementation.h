14ca6d0c290d8cc75d83b08b2c8b333a68a0eed9
Martin Prazak <martin.prazak@gmail.com>
2017-05-29 23:06:32 +0100

    Added NodeImplementation - a simple static registration hook for node code

---
+++ src/apps/possumwood/node_implementation.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <functional>
+#include <string>
+
+#include <boost/noncopyable.hpp>
+
+#include "metadata.h"
+
+/// a simple instantiation object, to be used to register a new node type in a cpp file
+struct NodeImplementation : public boost::noncopyable {
+	public:
+		NodeImplementation(const std::string& nodeName, std::function<void(Metadata&)> init);
+
+	private:
+		Metadata m_meta;
+};
