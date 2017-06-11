532f522faa3ec3627a01335716212ed6da9c5cc4
Martin Prazak <martin.prazak@gmail.com>
2017-06-12 00:01:11 +0100

    Fixed properties window refresh when opening a new scene

--- src/apps/possumwood/main_window.cpp
+++ src/apps/possumwood/main_window.cpp
@@ -116,8 +116,10 @@ MainWindow::MainWindow() : QMainWindow() {
 	newAct->setShortcuts(QKeySequence::New);
 	connect(newAct, &QAction::triggered, [&](bool) {
 		QMessageBox::StandardButton res = QMessageBox::question(this, "New file...", "Do you want to clear the scene?");
-		if(res == QMessageBox::Yes)
+		if(res == QMessageBox::Yes) {
+			m_properties->show({});
 			possumwood::App::instance().newFile();
+		}
 	});
 
 	QAction* openAct = new QAction("&Open...", this);
@@ -129,6 +131,7 @@ MainWindow::MainWindow() : QMainWindow() {
 
 		if(!filename.isEmpty()) {
 			try {
+				m_properties->show({});
 				possumwood::App::instance().loadFile(filename.toStdString());
 			}
 			catch(std::exception& err) {
