852a3bf06c215ae184e23fbdd72b002cab92c83f
Martin Prazak <martin.prazak@gmail.com>
2017-06-13 23:31:10 +0100

    Added edge length decimator module

--- src/plugins/openmesh/nodes/dec_edge_length.cpp
+++ src/plugins/openmesh/nodes/dec_edge_length.cpp
@@ -6,7 +6,7 @@
 #include <dependency_graph/port.inl>
 #include <dependency_graph/node.inl>
 
-#include <OpenMesh/Tools/Decimater/ModNormalDeviationT.hh>
+#include <OpenMesh/Tools/Decimater/ModEdgeLengthT.hh>
 
 #include "datatypes/decimater_module.h"
 
@@ -19,22 +19,21 @@ namespace {
 
 dependency_graph::InAttr<std::vector<DecimaterModule>> a_in;
 dependency_graph::InAttr<bool> a_binary;
-dependency_graph::InAttr<float> a_maxDeviation;
+dependency_graph::InAttr<float> a_edgeLength, a_errorFactor;
 dependency_graph::OutAttr<std::vector<DecimaterModule>> a_out;
 
 void compute(dependency_graph::Values& data) {
 	std::vector<DecimaterModule> decs = data.get(a_in);
-	bool binary = data.get(a_binary);
-	float maxDeviation = data.get(a_maxDeviation);
+	const bool binary = data.get(a_binary);
+	const float edgeLength = data.get(a_edgeLength);
+	const float errorFactor = data.get(a_errorFactor);
 
-	decs.push_back(DecimaterModule([binary, maxDeviation](OpenMesh::Decimater::DecimaterT<Mesh>& dec) {
-		if(!dec.mesh().has_face_normals())
-			dec.mesh().request_face_normals();
-
-		OpenMesh::Decimater::ModNormalDeviationT<Mesh>::Handle mod;
+	decs.push_back(DecimaterModule([binary, edgeLength, errorFactor](OpenMesh::Decimater::DecimaterT<Mesh>& dec) {
+		OpenMesh::Decimater::ModEdgeLengthT<Mesh>::Handle mod;
 		dec.add(mod);
 		dec.module(mod).set_binary(binary);
-		dec.module(mod).set_normal_deviation(maxDeviation);
+		dec.module(mod).set_edge_length(edgeLength);
+		dec.module(mod).set_error_tolerance_factor(errorFactor);
 	}));
 
 	data.set(a_out, decs);
@@ -43,16 +42,18 @@ void compute(dependency_graph::Values& data) {
 void init(possumwood::Metadata& meta) {
 	meta.addAttribute(a_in, "in_modules", std::vector<DecimaterModule>());
 	meta.addAttribute(a_binary, "binary", false);
-	meta.addAttribute(a_maxDeviation, "max_deviation", 15.0f);
+	meta.addAttribute(a_edgeLength, "edge_length", 0.2f);
+	meta.addAttribute(a_errorFactor, "error_factor", 0.0f);
 	meta.addAttribute(a_out, "out_modules", std::vector<DecimaterModule>());
 
 	meta.addInfluence(a_in, a_out);
 	meta.addInfluence(a_binary, a_out);
-	meta.addInfluence(a_maxDeviation, a_out);
+	meta.addInfluence(a_edgeLength, a_out);
+	meta.addInfluence(a_errorFactor, a_out);
 
 	meta.setCompute(compute);
 }
 
-possumwood::NodeImplementation s_impl("openmesh/decimater/module_normal_deviation", init);
+possumwood::NodeImplementation s_impl("openmesh/decimater/module_edge_length", init);
 
 }
