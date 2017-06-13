4ec88bc2ce9326963e3b3dd2860a216ef6e5f31b
Martin Prazak <martin.prazak@gmail.com>
2017-06-13 08:03:39 +0100

    Added a simple mesh transformation node

---
+++ src/plugins/openmesh/nodes/transform.cpp
@@ -0,0 +1,78 @@
+#include <possumwood_sdk/node_implementation.h>
+
+#include <dependency_graph/values.inl>
+#include <dependency_graph/attr.inl>
+#include <dependency_graph/datablock.inl>
+#include <dependency_graph/port.inl>
+#include <dependency_graph/node.inl>
+
+#include <OpenEXR/ImathVec.h>
+#include <OpenEXR/ImathEuler.h>
+
+#include "3d/io/vec3.h"
+
+#include "io/mesh.h"
+#include "openmesh.h"
+
+namespace {
+
+dependency_graph::InAttr<std::shared_ptr<const Mesh>> a_inMesh;
+dependency_graph::InAttr<Imath::Vec3<float>> a_translation, a_rotation, a_scale;
+dependency_graph::OutAttr<std::shared_ptr<const Mesh>> a_outMesh;
+
+void compute(dependency_graph::Values& data) {
+	const std::shared_ptr<const Mesh> mesh = data.get(a_inMesh);
+	const Imath::Vec3<float> tr = data.get(a_translation);
+	const Imath::Vec3<float> rot = data.get(a_rotation);
+	const Imath::Vec3<float> sc = data.get(a_scale);
+
+	if(mesh) {
+		std::unique_ptr<Mesh> newMesh(new Mesh(*mesh));
+
+		Imath::Matrix44<float> m1, m2, m3;
+		m1 = Imath::Euler<float>(Imath::Vec3<float>(
+			rot.x / M_PI * 180.0,
+			rot.y / M_PI * 180.0,
+			rot.z / M_PI * 180.0
+		)).toMatrix44();
+		m2.setScale(sc);
+		m3.setTranslation(tr);
+
+		const Imath::Matrix44<float> matrix = m1 * m2 * m3;
+
+		for(auto it = newMesh->vertices_begin(); it != newMesh->vertices_end(); ++it) {
+			Mesh::Point& pt = newMesh->point(*it);
+
+			Imath::Vec3<float> p(pt[0], pt[1], pt[2]);
+			p *= matrix;
+
+			pt[0] = p[0];
+			pt[1] = p[1];
+			pt[2] = p[2];
+		}
+
+		data.set(a_outMesh, std::shared_ptr<const Mesh>(newMesh.release()));
+	}
+	else
+		data.set(a_outMesh, std::shared_ptr<const Mesh>());
+
+}
+
+void init(possumwood::Metadata& meta) {
+	meta.addAttribute(a_inMesh, "in_mesh");
+	meta.addAttribute(a_translation, "translation");
+	meta.addAttribute(a_rotation, "rotation");
+	meta.addAttribute(a_scale, "scale", Imath::Vec3<float>(1, 1, 1));
+	meta.addAttribute(a_outMesh, "out_mesh");
+
+	meta.addInfluence(a_inMesh, a_outMesh);
+	meta.addInfluence(a_translation, a_outMesh);
+	meta.addInfluence(a_rotation, a_outMesh);
+	meta.addInfluence(a_scale, a_outMesh);
+
+	meta.setCompute(compute);
+}
+
+possumwood::NodeImplementation s_impl("openmesh/transform", init);
+
+}
