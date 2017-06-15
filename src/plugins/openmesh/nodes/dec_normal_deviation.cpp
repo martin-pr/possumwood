8f8688df743c82b1f5dd683665ad11980010d980
Martin Prazak <martin.prazak@gmail.com>
2017-06-15 22:10:31 +0100

    Added missing binary and error tolerance to aspect ration and normal deviation modules (respectively)

--- src/plugins/openmesh/nodes/dec_normal_deviation.cpp
+++ src/plugins/openmesh/nodes/dec_normal_deviation.cpp
@@ -19,15 +19,16 @@ namespace {
 
 dependency_graph::InAttr<std::vector<DecimaterModule>> a_in;
 dependency_graph::InAttr<bool> a_binary;
-dependency_graph::InAttr<float> a_maxDeviation;
+dependency_graph::InAttr<float> a_maxDeviation, a_errorFactor;
 dependency_graph::OutAttr<std::vector<DecimaterModule>> a_out;
 
 void compute(dependency_graph::Values& data) {
 	std::vector<DecimaterModule> decs = data.get(a_in);
 	bool binary = data.get(a_binary);
 	float maxDeviation = data.get(a_maxDeviation);
+	float errorFactor = data.get(a_errorFactor);
 
-	decs.push_back(DecimaterModule([binary, maxDeviation](OpenMesh::Decimater::DecimaterT<Mesh>& dec) {
+	decs.push_back(DecimaterModule([binary, maxDeviation, errorFactor](OpenMesh::Decimater::DecimaterT<Mesh>& dec) {
 		if(!dec.mesh().has_face_normals())
 			dec.mesh().request_face_normals();
 
@@ -35,6 +36,7 @@ void compute(dependency_graph::Values& data) {
 		dec.add(mod);
 		dec.module(mod).set_binary(binary);
 		dec.module(mod).set_normal_deviation(maxDeviation);
+		dec.module(mod).set_error_tolerance_factor(errorFactor);
 	}));
 
 	data.set(a_out, decs);
@@ -44,11 +46,13 @@ void init(possumwood::Metadata& meta) {
 	meta.addAttribute(a_in, "in_modules", std::vector<DecimaterModule>());
 	meta.addAttribute(a_binary, "binary", false);
 	meta.addAttribute(a_maxDeviation, "max_deviation", 15.0f);
+	meta.addAttribute(a_errorFactor, "error_factor", 0.0f);
 	meta.addAttribute(a_out, "out_modules", std::vector<DecimaterModule>());
 
 	meta.addInfluence(a_in, a_out);
 	meta.addInfluence(a_binary, a_out);
 	meta.addInfluence(a_maxDeviation, a_out);
+	meta.addInfluence(a_errorFactor, a_out);
 
 	meta.setCompute(compute);
 }
