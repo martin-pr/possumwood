9f10a1b5d092cca15f783acf145cc9dd17fc1165
Martin Prazak <martin.prazak@gmail.com>
2017-07-29 19:56:45 +0100

    Added a set of undo stack tests to make sure it behaves as expected

---
+++ src/tests/possumwood/undo_stack.cpp
@@ -0,0 +1,296 @@
+#include <boost/test/unit_test.hpp>
+
+#include <possumwood/undo_stack.h>
+
+namespace std {
+	std::ostream& operator << (std::ostream& out, const std::vector<unsigned>& vals) {
+		return out;
+	}
+}
+
+BOOST_AUTO_TEST_CASE(simple_undo_redo) {
+	std::vector<unsigned> values;
+
+	UndoStack stack;
+
+	{
+		UndoStack::Action a;
+		a.addCommand(
+			[&values]() {
+				values.push_back(10);
+			},
+			[&values]() {
+				BOOST_CHECK_EQUAL(values.back(), 10);
+				values.pop_back();
+			}
+		);
+		stack.execute(a);
+	}
+
+	BOOST_CHECK_EQUAL(values, decltype(values){10});
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, decltype(values){});
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, decltype(values){});
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, decltype(values){10});
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, decltype(values){10});
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, decltype(values){});
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, decltype(values){10});
+}
+
+BOOST_AUTO_TEST_CASE(multi_command_undo_redo) {
+	std::vector<unsigned> values;
+
+	UndoStack stack;
+
+	{
+		UndoStack::Action a;
+		a.addCommand(
+			[&values]() {
+				values.push_back(10);
+			},
+			[&values]() {
+				BOOST_CHECK_EQUAL(values.back(), 10);
+				values.pop_back();
+			}
+		);
+
+		a.addCommand(
+			[&values]() {
+				values.push_back(20);
+			},
+			[&values]() {
+				BOOST_CHECK_EQUAL(values.back(), 20);
+				values.pop_back();
+			}
+		);
+
+		stack.execute(a);
+	}
+
+	const std::vector<unsigned> expected_result{10, 20};
+
+	BOOST_CHECK_EQUAL(values, expected_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, decltype(values){});
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, decltype(values){});
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, expected_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, expected_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, decltype(values){});
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, expected_result);
+}
+
+BOOST_AUTO_TEST_CASE(multi_action_undo_redo) {
+	std::vector<unsigned> values;
+
+	UndoStack stack;
+
+	{
+		UndoStack::Action a;
+		a.addCommand(
+			[&values]() {
+				values.push_back(10);
+			},
+			[&values]() {
+				BOOST_CHECK_EQUAL(values.back(), 10);
+				values.pop_back();
+			}
+		);
+
+		a.addCommand(
+			[&values]() {
+				values.push_back(20);
+			},
+			[&values]() {
+				BOOST_CHECK_EQUAL(values.back(), 20);
+				values.pop_back();
+			}
+		);
+
+		stack.execute(a);
+	}
+
+	{
+		UndoStack::Action a;
+		a.addCommand(
+			[&values]() {
+				values.push_back(30);
+			},
+			[&values]() {
+				BOOST_CHECK_EQUAL(values.back(), 30);
+				values.pop_back();
+			}
+		);
+
+		a.addCommand(
+			[&values]() {
+				values.push_back(40);
+			},
+			[&values]() {
+				BOOST_CHECK_EQUAL(values.back(), 40);
+				values.pop_back();
+			}
+		);
+
+		stack.execute(a);
+	}
+	const std::vector<unsigned> full_result{10, 20, 30, 40};
+	const std::vector<unsigned> half_result{10, 20};
+	const std::vector<unsigned> empty_result{};
+
+	BOOST_CHECK_EQUAL(values, full_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, half_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, empty_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, empty_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, half_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, full_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, full_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, half_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, empty_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, empty_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, half_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, full_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, full_result);
+}
+
+BOOST_AUTO_TEST_CASE(exception_handling) {
+	std::vector<unsigned> values;
+
+	// 30 will never be inserted - need only 10 and 20
+	const std::vector<unsigned> half_result{10, 20};
+	const std::vector<unsigned> empty_result{};
+
+	UndoStack stack;
+
+	BOOST_CHECK_EQUAL(values, empty_result);
+
+	{
+
+		UndoStack::Action a;
+		a.addCommand(
+			[&values]() {
+				values.push_back(10);
+			},
+			[&values]() {
+				BOOST_CHECK_EQUAL(values.back(), 10);
+				values.pop_back();
+			}
+		);
+
+		a.addCommand(
+			[&values]() {
+				values.push_back(20);
+			},
+			[&values]() {
+				BOOST_CHECK_EQUAL(values.back(), 20);
+				values.pop_back();
+			}
+		);
+
+		BOOST_REQUIRE_NO_THROW(stack.execute(a));
+	}
+
+	BOOST_CHECK_EQUAL(values, half_result);
+
+	{
+		UndoStack::Action a;
+		a.addCommand(
+			[&values]() {
+				values.push_back(30);
+
+				const std::vector<unsigned> tmp{10, 20, 30};
+				BOOST_CHECK_EQUAL(values, tmp);
+			},
+			[&values]() {
+				BOOST_CHECK_EQUAL(values.back(), 30);
+				values.pop_back();
+
+				const std::vector<unsigned> tmp{10, 20};
+				BOOST_CHECK_EQUAL(values, tmp);
+			}
+		);
+
+		a.addCommand(
+			[&values]() {
+				throw "stuff";
+			},
+			[&values]() {
+			}
+		);
+
+		// executing this command throws, and UNROLLS push_back of 30
+		BOOST_REQUIRE_THROW(stack.execute(a), const char*);
+		BOOST_REQUIRE_THROW(stack.execute(a), const char*);
+	}
+
+	BOOST_CHECK_EQUAL(values, half_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, empty_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, empty_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, half_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, half_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, empty_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.undo());
+	BOOST_CHECK_EQUAL(values, empty_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, half_result);
+
+	BOOST_REQUIRE_NO_THROW(stack.redo());
+	BOOST_CHECK_EQUAL(values, half_result);
+}
