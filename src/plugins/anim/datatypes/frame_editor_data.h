a58e6912c0175583b57352db607145108bef1325
Martin Prazak <martin.prazak@gmail.com>
2017-09-08 23:28:32 +0100

    A simple frame editor + UI, allowing to adjust a skeleton (or a frame) by bone transforms

---
+++ src/plugins/anim/datatypes/frame_editor_data.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "skeleton.h"
+
+namespace anim {
+
+/// A simple data container for frame editor node - holds individual transformations for each bone.
+class FrameEditorData {
+  public:
+	void setSkeleton(const Skeleton& s);
+	const Skeleton& skeleton() const;
+
+	void setTransform(std::size_t joint, const Transform& tr);
+	void resetTransform(std::size_t joint);
+	Transform transform(std::size_t joint) const;
+	void clear();
+
+	typedef std::map<std::size_t, Transform>::const_iterator const_iterator;
+	const_iterator begin() const;
+	const_iterator end() const;
+
+	std::size_t size() const;
+
+	bool operator==(const FrameEditorData& d) const;
+	bool operator!=(const FrameEditorData& d) const;
+
+  protected:
+  private:
+  	// this should be only a list of bones!
+	Skeleton m_skeleton;
+	std::map<std::size_t, Transform> m_transforms;
+};
+}
+
+namespace possumwood {
+
+template<>
+struct Traits<anim::FrameEditorData> {
+	static IO<anim::FrameEditorData> io;
+
+	static constexpr std::array<float, 3> colour() {
+		return std::array<float, 3>{{0, 0.2, 0}};
+	}
+};
+
+}
--- src/plugins/anim/nodes/frame_edit.cpp
+++ src/plugins/anim/nodes/frame_edit.cpp
@@ -10,20 +10,34 @@
 #include <OpenEXR/ImathEuler.h>
 
 #include "datatypes/animation.h"
+#include "datatypes/frame_editor_data.h"
 #include "3d/io/vec3.h"
 
 namespace {
 
 dependency_graph::InAttr<anim::Skeleton> a_inFrame;
 dependency_graph::InAttr<Imath::Vec3<float>> a_rootTranslation;
+dependency_graph::InAttr<anim::FrameEditorData> a_editorData;
 dependency_graph::OutAttr<anim::Skeleton> a_outFrame;
 
 dependency_graph::State compute(dependency_graph::Values& data) {
+	// update a_editorData, if needed
+	if(data.get(a_editorData).skeleton() != data.get(a_inFrame)) {
+		anim::FrameEditorData editorData = data.get(a_editorData);
+		editorData.setSkeleton(data.get(a_inFrame));
+		data.set(a_editorData, editorData);
+	}
+
+	// do the computation itself
 	anim::Skeleton frame = data.get(a_inFrame);
 	const Imath::Vec3<float>& rootTr = data.get(a_rootTranslation);
 
-	if(frame.size() > 0)
+	if(frame.size() > 0) {
 		frame[0].tr().translation += rootTr;
+
+		for(auto& i : data.get(a_editorData))
+			frame[i.first].tr() = i.second * frame[i.first].tr();
+	}
 
 	data.set(a_outFrame, frame);
 
@@ -34,9 +48,11 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 void init(possumwood::Metadata& meta) {
 	meta.addAttribute(a_inFrame, "in_frame");
 	meta.addAttribute(a_rootTranslation, "root_translate", Imath::Vec3<float>(0, 0, 0));
+	meta.addAttribute(a_editorData, "rotations");
 	meta.addAttribute(a_outFrame, "out_frame");
 
 	meta.addInfluence(a_inFrame, a_outFrame);
+	meta.addInfluence(a_editorData, a_outFrame);
 	meta.addInfluence(a_rootTranslation, a_outFrame);
 
 	meta.setCompute(compute);
