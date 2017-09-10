d05b4eb940d5b226d389c0d894b921e3094d9bbc
Martin Prazak <martin.prazak@gmail.com>
2017-09-10 01:02:27 +0100

    Frame edit - added support for scale

--- src/plugins/anim/nodes/frame_edit.cpp
+++ src/plugins/anim/nodes/frame_edit.cpp
@@ -8,6 +8,7 @@
 
 #include <OpenEXR/ImathVec.h>
 #include <OpenEXR/ImathEuler.h>
+#include <OpenEXR/ImathMatrix.h>
 
 #include "datatypes/animation.h"
 #include "datatypes/frame_editor_data.h"
@@ -16,7 +17,8 @@
 namespace {
 
 dependency_graph::InAttr<anim::Skeleton> a_inFrame;
-dependency_graph::InAttr<Imath::Vec3<float>> a_rootTranslation;
+dependency_graph::InAttr<Imath::Vec3<float>> a_translate;
+dependency_graph::InAttr<float> a_scale;
 dependency_graph::InAttr<anim::FrameEditorData> a_editorData;
 dependency_graph::OutAttr<anim::Skeleton> a_outFrame;
 
@@ -30,10 +32,14 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 
 	// do the computation itself
 	anim::Skeleton frame = data.get(a_inFrame);
-	const Imath::Vec3<float>& rootTr = data.get(a_rootTranslation);
+	const Imath::Vec3<float>& tr = data.get(a_translate);
+	const float sc = data.get(a_scale);
 
 	if(frame.size() > 0) {
-		frame[0].tr().translation += rootTr;
+		for(auto& b : frame)
+			b.tr().translation *= sc;
+
+		frame[0].tr().translation += tr;
 
 		for(auto& i : data.get(a_editorData))
 			frame[i.first].tr() = i.second * frame[i.first].tr();
@@ -47,13 +53,15 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 
 void init(possumwood::Metadata& meta) {
 	meta.addAttribute(a_inFrame, "in_frame");
-	meta.addAttribute(a_rootTranslation, "root_translate", Imath::Vec3<float>(0, 0, 0));
+	meta.addAttribute(a_translate, "translate", Imath::Vec3<float>(0, 0, 0));
+	meta.addAttribute(a_scale, "scale", 1.0f);
 	meta.addAttribute(a_editorData, "rotations");
 	meta.addAttribute(a_outFrame, "out_frame");
 
 	meta.addInfluence(a_inFrame, a_outFrame);
 	meta.addInfluence(a_editorData, a_outFrame);
-	meta.addInfluence(a_rootTranslation, a_outFrame);
+	meta.addInfluence(a_translate, a_outFrame);
+	meta.addInfluence(a_scale, a_outFrame);
 
 	meta.setCompute(compute);
 }
