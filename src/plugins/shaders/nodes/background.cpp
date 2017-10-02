679b8117e50d2aeaf23429b56cc65c93500e4e10
Martin Prazak <martin.prazak@gmail.com>
2017-10-03 00:22:32 +0100

    Added modelview and projection matrix uniforms to background shader Cleaned up the code Added a more funky example with raymarched cubes

--- src/plugins/shaders/nodes/background.cpp
+++ src/plugins/shaders/nodes/background.cpp
@@ -160,15 +160,19 @@ struct Drawable : public possumwood::Drawable {
 			const GLchar* src =
 				"#version 330\n"
 				"\n"
+				"// input position from the CPU\n"
 				"in vec3 position;\n"
 				"\n"
+				"// near and far per-vertex world positions, useable for raytracing in the fragment shader\n"
 				"in vec3 iNearPositionVert;\n"
 				"in vec3 iFarPositionVert;\n"
 				"out vec3 iNearPosition;\n"
 				"out vec3 iFarPosition;\n"
 				"\n"
 				"void main() {\n"
+				"	// do not do any transformation - this should lead to a single quad covering the whole viewport\n"
 				"	gl_Position = vec4(position.x, position.y, position.z, 1); \n"
+				"	// just pass the near and far positions - they'll get linearly interpolated\n"
 				"	iNearPosition = iNearPositionVert;\n"
 				"	iFarPosition = iFarPositionVert;\n"
 				"}";
@@ -264,13 +268,36 @@ struct Drawable : public possumwood::Drawable {
 			double modelview[16];
 			glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
 
+			{
+				// modelview matrix
+				GLint iModelViewAttr = glGetUniformLocation(m_programId, "iModelView");
+				if(iModelViewAttr >= 0) {
+					float modelviewf[16];
+					for(unsigned a=0;a<16;++a)
+						modelviewf[a] = modelview[a];
+					glUniformMatrix4fv(iModelViewAttr, 1, false, modelviewf);
+				}
+			}
+
 			double projection[16];
 			glGetDoublev(GL_PROJECTION_MATRIX, projection);
+
+			{
+				// projection matrix
+				GLint iProjectionAttr = glGetUniformLocation(m_programId, "iProjection");
+				if(iProjectionAttr >= 0) {
+					float projectionf[16];
+					for(unsigned a=0;a<16;++a)
+						projectionf[a] = projection[a];
+					glUniformMatrix4fv(iProjectionAttr, 1, false, projectionf);
+				}
+			}
 
 			GLint viewport[4];
 			glGetIntegerv(GL_VIEWPORT, viewport);
 
 			{
+				// points on the near plane, corresponding to each fragment (useful for raytracing)
 				double nearPosData[12];
 				gluUnProject(0, 0, 0, modelview, projection, viewport, nearPosData, nearPosData+1, nearPosData+2);
 				gluUnProject(width(), 0, 0, modelview, projection, viewport, nearPosData+3, nearPosData+4, nearPosData+5);
@@ -293,6 +320,7 @@ struct Drawable : public possumwood::Drawable {
 			}
 
 			{
+				// points on the far plane, corresponding to each fragment (useful for raytracing)
 				double farPosData[12];
 				gluUnProject(0, 0, 1, modelview, projection, viewport, farPosData, farPosData+1, farPosData+2);
 				gluUnProject(width(), 0, 1, modelview, projection, viewport, farPosData+3, farPosData+4, farPosData+5);
@@ -315,22 +343,12 @@ struct Drawable : public possumwood::Drawable {
 			}
 
 			{
+				// viewport resolution - useful for screen-space shaders
 				GLint attr = glGetUniformLocation(m_programId, "iResolution");
 				if(attr >= 0) {
 					float res[2] = {(float)width(), (float)height()};
 					glUniform2fv(attr, 1, res);
 				}
-			}
-
-			const double near = (2.0*projection[14])/(2.0*projection[10]-2.0);
-			GLint nearAttr = glGetUniformLocation(m_programId, "iNear");
-			if(nearAttr >= 0)
-				glUniform1d(nearAttr, near);
-
-			GLint farAttr = glGetUniformLocation(m_programId, "iFar");
-			if(farAttr >= 0) {
-				const double far = ((projection[10]-1.0)*near)/(projection[10]+1.0);
-				glUniform1f(farAttr, far);
 			}
 
 			// and execute draw
@@ -362,15 +380,74 @@ void init(possumwood::Metadata& meta) {
 	meta.addAttribute(a_src, "source", std::string(
 		"#version 330\n"
 		"\n"
-		"uniform mat4 iProjectionMat;\n"
-		"uniform mat4 iProjectionMatInv;\n"
-		"uniform vec2 iResolution;\n"
+		"// generic attributes\n"
+		"uniform mat4 iProjection;  // projection matrix\n"
+		"uniform mat4 iModelView;   // modelview matrix\n"
+		"uniform vec2 iResolution;  // viewport resolution\n"
 		"\n"
+		"// attributes useable for raytracing\n"
+		"in vec3 iNearPosition;     // position of fragment-corresponding point on near plane\n"
+		"in vec3 iFarPosition;      // position of fragment-corresponding point on far plane\n"
+		"\n"
+		"// output colour\n"
 		"layout(location=0) out vec4 color;\n"
 		"\n"
+		"// a simple integer-based checkerboard pattern\n"
+		"float tile(vec2 pos) {\n"
+		"	return (\n"
+		"		int(pos.x + max(0.0, sign(pos.x))) + \n"
+		"		int(pos.y + max(0.0, sign(pos.y)))\n"
+		"	) % 2;\n"
+		"}\n"
+		"\n"
+		"// computes Z-buffer depth value, and converts the range.\n"
+		"// ref: https://stackoverflow.com/questions/10264949/glsl-gl-fragcoord-z-calculation-and-setting-gl-fragdepth\n"
+		"float computeDepth(vec3 pos) {\n"
+		"	// get the clip-space coordinates\n"
+		"	vec4 eye_space_pos = iModelView * vec4(pos.xyz, 1.0);\n"
+		"	vec4 clip_space_pos = iProjection * eye_space_pos;\n"
+		"\n"
+		"	// get the depth value in normalized device coordinates\n"
+		"	float ndc_depth = clip_space_pos.z / clip_space_pos.w;\n"
+		"\n"
+		"	// and compute the range based on gl_DepthRange settings (usually not necessary, but still)\n"
+		"	float far = gl_DepthRange.far; \n"
+		"	float near = gl_DepthRange.near;\n"
+		"\n"
+		"	float depth = (((far-near) * ndc_depth) + near + far) / 2.0;\n"
+		"\n"
+		"	// and return the result\n"
+		"	return depth;\n"
+		"}\n"
+		"\n"
 		"void main() {\n"
-		"	color = vec4(1,0,1,1);\n"
-		"}\n"));
+		"	// find the t parameter where Y = 0 (intersection with ground plane)\n"
+		"	float t = iNearPosition.y / (iNearPosition.y - iFarPosition.y);\n"
+		"\n"
+		"	// not intersecting with ground plane at all - discard\n"
+		"	if(t < 0.0)\n"
+		"		discard;\n"
+		"\n"
+		"	// find the intersecting position\n"
+		"	vec3 pos = iNearPosition + t * (iFarPosition - iNearPosition);\n"
+		"\n"
+		"	// and make the checkerboard pattern\n"
+		"	float col = tile(pos.xz) * 0.3;\n"
+		"	col = col + tile(pos.xz / 10.0) * 0.15;\n"
+		"	col = col + tile(pos.xz / 100.0) * 0.075;\n"
+		"\n"
+		"	// simple attenuation with distance\n"
+		"	float dist = sqrt(pos.x*pos.x + pos.z*pos.z);\n"
+		"	float dim = 1.0 - dist / (dist + 100.0);\n"
+		"	col = col * dim;\n"
+		"\n"
+		"	// output colour\n"
+		"	color = vec4(col, col, col, 1);\n"
+		"\n"
+		"	// convert the world-space position to a depth value, to keep Z buffer working\n"
+		"	gl_FragDepth = computeDepth(pos);\n"
+		"}\n"
+	));
 
 	meta.setDrawable<Drawable>();
 	meta.setEditor<Editor>();
