ab93ba9ee7bae5960abad84e0bd436af0fc68c46
Martin Prazak <martin.prazak@gmail.com>
2017-10-02 22:53:30 +0100

    Sending additional near and far plane positions in world space to support raycasting; added simple checker floot "background" shader

--- src/plugins/shaders/nodes/background.cpp
+++ src/plugins/shaders/nodes/background.cpp
@@ -159,9 +159,18 @@ struct Drawable : public possumwood::Drawable {
 			m_vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
 			const GLchar* src =
 				"#version 330\n"
+				"\n"
 				"in vec3 position;\n"
+				"\n"
+				"in vec3 iNearPositionVert;\n"
+				"in vec3 iFarPositionVert;\n"
+				"out vec3 iNearPosition;\n"
+				"out vec3 iFarPosition;\n"
+				"\n"
 				"void main() {\n"
 				"	gl_Position = vec4(position.x, position.y, position.z, 1); \n"
+				"	iNearPosition = iNearPositionVert;\n"
+				"	iFarPosition = iFarPositionVert;\n"
 				"}";
 			glShaderSource(m_vertexShaderId, 1, &src, 0);
 			glCompileShader(m_vertexShaderId);
@@ -252,36 +261,56 @@ struct Drawable : public possumwood::Drawable {
 			glUseProgram(m_programId);
 
 			// feed in the uniforms
+			double modelview[16];
+			glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+
+			double projection[16];
+			glGetDoublev(GL_PROJECTION_MATRIX, projection);
+
+			GLint viewport[4];
+			glGetIntegerv(GL_VIEWPORT, viewport);
+
 			{
-				Imath::M44f model, projection;
-				{
-					float tmp[16];
+				double nearPosData[12];
+				gluUnProject(0, 0, 0, modelview, projection, viewport, nearPosData, nearPosData+1, nearPosData+2);
+				gluUnProject(width(), 0, 0, modelview, projection, viewport, nearPosData+3, nearPosData+4, nearPosData+5);
+				gluUnProject(width(), height(), 0, modelview, projection, viewport, nearPosData+6, nearPosData+7, nearPosData+8);
+				gluUnProject(0, height(), 0, modelview, projection, viewport, nearPosData+9, nearPosData+10, nearPosData+11);
 
-					glGetFloatv(GL_MODELVIEW_MATRIX, tmp);
-					for(unsigned a = 0; a<16; ++a)
-						model[a / 4][a % 4] = tmp[a];
+				if(m_nearPosBuffer == 0)
+					glGenBuffers(1, &m_nearPosBuffer);
+				glBindBuffer(GL_ARRAY_BUFFER, m_nearPosBuffer);
 
-					glGetFloatv(GL_PROJECTION_MATRIX, tmp);
-					for(unsigned a = 0; a<16; ++a)
-						projection[a / 4][a % 4] = tmp[a];
+				glBufferData(GL_ARRAY_BUFFER, sizeof(nearPosData), nearPosData, GL_STATIC_DRAW);
+
+				GLint iNearPosAttr = glGetAttribLocation(m_programId, "iNearPositionVert");
+				if(iNearPosAttr >= 0) {
+					glEnableVertexAttribArray(iNearPosAttr);
+					glVertexAttribPointer(iNearPosAttr, 3, GL_DOUBLE, 0, 0, 0);
 				}
 
-				Imath::M44f modelviewProjection = model * projection;
-				GLint projectionMatrixAttr = glGetUniformLocation(m_programId, "iProjectionMat");
-				if(projectionMatrixAttr >= 0) {
-					float tmp[16];
-					for(unsigned a = 0; a<16; ++a)
-						tmp[a] = projection[a / 4][a % 4];
-					glUniformMatrix4fv(projectionMatrixAttr, 1, GL_FALSE, tmp);
-				}
+				glBindBuffer(GL_ARRAY_BUFFER, 0);
+			}
 
-				modelviewProjection.invert();
-				GLint projectionMatrixInvAttr = glGetUniformLocation(m_programId, "iProjectionMatInv");
-				if(projectionMatrixInvAttr >= 0) {
-					float tmp[16];
-					for(unsigned a = 0; a<16; ++a)
-						tmp[a] = projection[a / 4][a % 4];
-					glUniformMatrix4fv(projectionMatrixInvAttr, 1, GL_FALSE, tmp);
+			{
+				double farPosData[12];
+				gluUnProject(0, 0, 1, modelview, projection, viewport, farPosData, farPosData+1, farPosData+2);
+				gluUnProject(width(), 0, 1, modelview, projection, viewport, farPosData+3, farPosData+4, farPosData+5);
+				gluUnProject(width(), height(), 1, modelview, projection, viewport, farPosData+6, farPosData+7, farPosData+8);
+				gluUnProject(0, height(), 1, modelview, projection, viewport, farPosData+9, farPosData+10, farPosData+11);
+
+				if(m_farPosBuffer == 0)
+					glGenBuffers(1, &m_farPosBuffer);
+				glBindBuffer(GL_ARRAY_BUFFER, m_farPosBuffer);
+
+				glBufferData(GL_ARRAY_BUFFER, sizeof(farPosData), farPosData, GL_STATIC_DRAW);
+
+				GLint iFarPosAttr = glGetAttribLocation(m_programId, "iFarPositionVert");
+				if(iFarPosAttr >= 0) {
+					glEnableVertexAttribArray(iFarPosAttr);
+					glVertexAttribPointer(iFarPosAttr, 3, GL_DOUBLE, 0, 0, 0);
+
+					glBindBuffer(GL_ARRAY_BUFFER, 0);
 				}
 			}
 
@@ -291,6 +320,17 @@ struct Drawable : public possumwood::Drawable {
 					float res[2] = {(float)width(), (float)height()};
 					glUniform2fv(attr, 1, res);
 				}
+			}
+
+			const double near = (2.0*projection[14])/(2.0*projection[10]-2.0);
+			GLint nearAttr = glGetUniformLocation(m_programId, "iNear");
+			if(nearAttr >= 0)
+				glUniform1d(nearAttr, near);
+
+			GLint farAttr = glGetUniformLocation(m_programId, "iFar");
+			if(farAttr >= 0) {
+				const double far = ((projection[10]-1.0)*near)/(projection[10]+1.0);
+				glUniform1f(farAttr, far);
 			}
 
 			// and execute draw
@@ -312,6 +352,8 @@ struct Drawable : public possumwood::Drawable {
 
 	GLuint m_vao = 0;
 	GLuint m_posBuffer = 0;
+	GLuint m_nearPosBuffer = 0;
+	GLuint m_farPosBuffer = 0;
 
 	std::string m_currentFragmentSource;
 };
