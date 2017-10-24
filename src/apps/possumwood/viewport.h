0ff104e0d76a4890a3fd3985b22e2045bb12e197
Martin Prazak <martin.prazak@gmail.com>
2017-10-24 22:38:51 +0100

    Added mouse viewport translation

--- src/apps/possumwood/viewport.h
+++ src/apps/possumwood/viewport.h
@@ -10,22 +10,23 @@
 class Viewport : public QGLWidget, public boost::noncopyable {
 	Q_OBJECT
 
-	signals:
-		void render(float dt);
+  signals:
+	void render(float dt);
 
-	public:
-		Viewport(QWidget* parent = NULL);
-		virtual ~Viewport();
+  public:
+	Viewport(QWidget* parent = NULL);
+	virtual ~Viewport();
 
-	protected:
-		virtual void initializeGL();
-		virtual void paintGL();
-		virtual void resizeGL(int w, int h);
-		virtual void mouseMoveEvent(QMouseEvent* event);
+  protected:
+	virtual void initializeGL();
+	virtual void paintGL();
+	virtual void resizeGL(int w, int h);
+	virtual void mouseMoveEvent(QMouseEvent* event);
 
-	private:
-		float m_sceneDistance, m_sceneRotationX, m_sceneRotationY;
-		int m_mouseX, m_mouseY;
+  private:
+	float m_sceneDistance, m_sceneRotationX, m_sceneRotationY;
+	float m_originX, m_originY, m_originZ;
+	int m_mouseX, m_mouseY;
 
-		boost::posix_time::ptime m_timer;
+	boost::posix_time::ptime m_timer;
 };
