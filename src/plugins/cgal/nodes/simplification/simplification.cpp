47796f90187dd493fe885052599910b5c474e964
Martin Prazak <martin.prazak@gmail.com>
2017-10-31 22:44:35 +0000

    Added cost metric dropdown to CGAL mesh simplification

--- src/plugins/cgal/nodes/simplification/simplification.cpp
+++ src/plugins/cgal/nodes/simplification/simplification.cpp
@@ -9,6 +9,8 @@
 #include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Edge_length_stop_predicate.h>
 
 #include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Edge_length_cost.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/LindstromTurk_cost.h>
+
 #include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Midpoint_placement.h>
 
 #include "possumwood_sdk/datatypes/enum.h"
@@ -24,33 +26,50 @@ namespace {
 
 dependency_graph::InAttr<std::shared_ptr<const possumwood::CGALPolyhedron>> a_inMesh;
 dependency_graph::InAttr<float> a_stopParam;
-dependency_graph::InAttr<possumwood::Enum> a_stopCondition;
+dependency_graph::InAttr<possumwood::Enum> a_stopCondition, a_cost;
 dependency_graph::OutAttr<std::shared_ptr<const possumwood::CGALPolyhedron>> a_outMesh;
 
 dependency_graph::State compute(dependency_graph::Values& data) {
 	std::shared_ptr<const possumwood::CGALPolyhedron> inMesh = data.get(a_inMesh);
 	const float stopCondition = data.get(a_stopParam);
-	const unsigned algorithmId = data.get(a_stopCondition).intValue();
+	const unsigned algorithmId = data.get(a_stopCondition).intValue() + data.get(a_cost).intValue();
 
 	if(inMesh) {
 		std::unique_ptr<possumwood::CGALPolyhedron> mesh(new possumwood::CGALPolyhedron(*inMesh));
 
 		switch(algorithmId) {
-			case 1:
+			case 11:
 				SMS::edge_collapse(*mesh, SMS::Count_stop_predicate<possumwood::CGALPolyhedron>(stopCondition),
 				                   CGAL::parameters::get_cost(SMS::Edge_length_cost<possumwood::CGALPolyhedron>())
 				                       .get_placement(SMS::Midpoint_placement<possumwood::CGALPolyhedron>()));
 				break;
 
-			case 2:
+			case 12:
 				SMS::edge_collapse(*mesh, SMS::Count_ratio_stop_predicate<possumwood::CGALPolyhedron>(stopCondition),
 				                   CGAL::parameters::get_cost(SMS::Edge_length_cost<possumwood::CGALPolyhedron>())
 				                       .get_placement(SMS::Midpoint_placement<possumwood::CGALPolyhedron>()));
 				break;
 
-			case 3:
+			case 13:
 				SMS::edge_collapse(*mesh, SMS::Edge_length_stop_predicate<float>(stopCondition),
 				                   CGAL::parameters::get_cost(SMS::Edge_length_cost<possumwood::CGALPolyhedron>())
+				                       .get_placement(SMS::Midpoint_placement<possumwood::CGALPolyhedron>()));
+				break;
+			case 21:
+				SMS::edge_collapse(*mesh, SMS::Count_stop_predicate<possumwood::CGALPolyhedron>(stopCondition),
+				                   CGAL::parameters::get_cost(SMS::LindstromTurk_cost<possumwood::CGALPolyhedron>())
+				                       .get_placement(SMS::Midpoint_placement<possumwood::CGALPolyhedron>()));
+				break;
+
+			case 22:
+				SMS::edge_collapse(*mesh, SMS::Count_ratio_stop_predicate<possumwood::CGALPolyhedron>(stopCondition),
+				                   CGAL::parameters::get_cost(SMS::LindstromTurk_cost<possumwood::CGALPolyhedron>())
+				                       .get_placement(SMS::Midpoint_placement<possumwood::CGALPolyhedron>()));
+				break;
+
+			case 23:
+				SMS::edge_collapse(*mesh, SMS::Edge_length_stop_predicate<float>(stopCondition),
+				                   CGAL::parameters::get_cost(SMS::LindstromTurk_cost<possumwood::CGALPolyhedron>())
 				                       .get_placement(SMS::Midpoint_placement<possumwood::CGALPolyhedron>()));
 				break;
 		}
@@ -69,11 +88,14 @@ void init(possumwood::Metadata& meta) {
 	    a_stopCondition, "stop_condition",
 	    possumwood::Enum({std::make_pair("Count_stop_predicate", 1), std::make_pair("Count_ratio_stop_predicate", 2),
 	                      std::make_pair("Edge_length_stop_predicate", 3)}));
+	meta.addAttribute(a_cost, "cost", possumwood::Enum({std::make_pair("Edge_length_cost", 10),
+	                                                    std::make_pair("LindstromTurk_cost", 20)}));
 	meta.addAttribute(a_stopParam, "stop_parameter", 1000.0f);
 	meta.addAttribute(a_outMesh, "output");
 
 	meta.addInfluence(a_inMesh, a_outMesh);
 	meta.addInfluence(a_stopCondition, a_outMesh);
+	meta.addInfluence(a_cost, a_outMesh);
 	meta.addInfluence(a_stopParam, a_outMesh);
 
 	meta.setCompute(compute);
