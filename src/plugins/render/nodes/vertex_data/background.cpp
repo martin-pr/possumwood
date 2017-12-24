ad9a37c422fbe653acd6b23648c65f3d1b31e6a8
Martin <martin.prazak@gmail.com>
2017-12-24 13:14:05 +0000

    Background vertex data node now uses matrices from Drawable::ViewportState

--- src/plugins/render/nodes/vertex_data/background.cpp
+++ src/plugins/render/nodes/vertex_data/background.cpp
@@ -10,6 +10,14 @@
 
 namespace {
 
+Imath::M44d toDouble(const Imath::M44f& matrix) {
+	Imath::M44d result;
+	for(unsigned a = 0; a < 16; ++a)
+		result.getValue()[a] = matrix.getValue()[a];
+
+	return result;
+}
+
 dependency_graph::OutAttr<std::shared_ptr<const possumwood::VertexData>> a_vd;
 
 dependency_graph::State compute(dependency_graph::Values& data) {
@@ -18,63 +26,66 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 	// we're drawing quads
 	std::unique_ptr<possumwood::VertexData> vd(new possumwood::VertexData(GL_QUADS));
 
-	vd->addVBO<Imath::V3f>(
-		"position",
-		4,
-		possumwood::VertexData::kStatic,
-		[](possumwood::Buffer<float>& buffer, const possumwood::Drawable::ViewportState& viewport) {
-			buffer.element(0) = Imath::V3f(-1,-1,1);
-			buffer.element(1) = Imath::V3f(1,-1,1);
-			buffer.element(2) = Imath::V3f(1,1,1);
-			buffer.element(3) = Imath::V3f(-1,1,1);
-		}
-	);
+	vd->addVBO<Imath::V3f>("position", 4, possumwood::VertexData::kStatic,
+	                       [](possumwood::Buffer<float>& buffer,
+	                          const possumwood::Drawable::ViewportState& viewport) {
+		                       buffer.element(0) = Imath::V3f(-1, -1, 1);
+		                       buffer.element(1) = Imath::V3f(1, -1, 1);
+		                       buffer.element(2) = Imath::V3f(1, 1, 1);
+		                       buffer.element(3) = Imath::V3f(-1, 1, 1);
+		                   });
 
 	vd->addVBO<Imath::V3d>(
-		"iNearPositionVert",
-		4,
-		possumwood::VertexData::kPerDraw,
-		[](possumwood::Buffer<double>& buffer, const possumwood::Drawable::ViewportState& vp) {
-			GLint viewport[4];
-			glGetIntegerv(GL_VIEWPORT, viewport);
+	    "iNearPositionVert", 4, possumwood::VertexData::kPerDraw,
+	    [](possumwood::Buffer<double>& buffer,
+	       const possumwood::Drawable::ViewportState& vp) {
+		    GLint viewport[4];
+		    viewport[0] = 0;
+		    viewport[1] = 0;
+		    viewport[2] = vp.width;
+		    viewport[3] = vp.height;
 
-			double modelview[16];
-			glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+		    const Imath::M44d modelview = toDouble(vp.modelview);
+		    const Imath::M44d projection = toDouble(vp.projection);
 
-			double projection[16];
-			glGetDoublev(GL_PROJECTION_MATRIX, projection);
-
-			// points on the near plane, corresponding to each fragment (useful for raytracing)
-			double* ptr = buffer.element(0).ptr();
-			gluUnProject(0, 0, 0, modelview, projection, viewport, ptr, ptr+1, ptr+2);
-			gluUnProject(viewport[2], 0, 0, modelview, projection, viewport, ptr+3, ptr+4, ptr+5);
-			gluUnProject(viewport[2], viewport[3], 0, modelview, projection, viewport, ptr+6, ptr+7, ptr+8);
-			gluUnProject(0, viewport[3], 0, modelview, projection, viewport, ptr+9, ptr+10, ptr+11);
-		}
-	);
+		    // points on the near plane, corresponding to each fragment (useful for
+		    // raytracing)
+		    double* ptr = buffer.element(0).ptr();
+		    gluUnProject(0, 0, 0, modelview.getValue(), projection.getValue(), viewport,
+		                 ptr, ptr + 1, ptr + 2);
+		    gluUnProject(viewport[2], 0, 0, modelview.getValue(), projection.getValue(),
+		                 viewport, ptr + 3, ptr + 4, ptr + 5);
+		    gluUnProject(viewport[2], viewport[3], 0, modelview.getValue(),
+		                 projection.getValue(), viewport, ptr + 6, ptr + 7, ptr + 8);
+		    gluUnProject(0, viewport[3], 0, modelview.getValue(), projection.getValue(),
+		                 viewport, ptr + 9, ptr + 10, ptr + 11);
+		});
 
 	vd->addVBO<Imath::V3d>(
-		"iFarPositionVert",
-		4,
-		possumwood::VertexData::kPerDraw,
-		[](possumwood::Buffer<double>& buffer, const possumwood::Drawable::ViewportState& vp) {
-			GLint viewport[4];
-			glGetIntegerv(GL_VIEWPORT, viewport);
+	    "iFarPositionVert", 4, possumwood::VertexData::kPerDraw,
+	    [](possumwood::Buffer<double>& buffer,
+	       const possumwood::Drawable::ViewportState& vp) {
+		    GLint viewport[4];
+		    viewport[0] = 0;
+		    viewport[1] = 0;
+		    viewport[2] = vp.width;
+		    viewport[3] = vp.height;
 
-			double modelview[16];
-			glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+		    const Imath::M44d modelview = toDouble(vp.modelview);
+		    const Imath::M44d projection = toDouble(vp.projection);
 
-			double projection[16];
-			glGetDoublev(GL_PROJECTION_MATRIX, projection);
-
-			// points on the near plane, corresponding to each fragment (useful for raytracing)
-			double* ptr = buffer.element(0).ptr();
-			gluUnProject(0, 0, 1, modelview, projection, viewport, ptr, ptr+1, ptr+2);
-			gluUnProject(viewport[2], 0, 1, modelview, projection, viewport, ptr+3, ptr+4, ptr+5);
-			gluUnProject(viewport[2], viewport[3], 1, modelview, projection, viewport, ptr+6, ptr+7, ptr+8);
-			gluUnProject(0, viewport[3], 1, modelview, projection, viewport, ptr+9, ptr+10, ptr+11);
-		}
-	);
+		    // points on the near plane, corresponding to each fragment (useful for
+		    // raytracing)
+		    double* ptr = buffer.element(0).ptr();
+		    gluUnProject(0, 0, 1, modelview.getValue(), projection.getValue(), viewport,
+		                 ptr, ptr + 1, ptr + 2);
+		    gluUnProject(viewport[2], 0, 1, modelview.getValue(), projection.getValue(),
+		                 viewport, ptr + 3, ptr + 4, ptr + 5);
+		    gluUnProject(viewport[2], viewport[3], 1, modelview.getValue(),
+		                 projection.getValue(), viewport, ptr + 6, ptr + 7, ptr + 8);
+		    gluUnProject(0, viewport[3], 1, modelview.getValue(), projection.getValue(),
+		                 viewport, ptr + 9, ptr + 10, ptr + 11);
+		});
 
 	data.set(a_vd, std::shared_ptr<const possumwood::VertexData>(vd.release()));
 
@@ -88,5 +99,4 @@ void init(possumwood::Metadata& meta) {
 }
 
 possumwood::NodeImplementation s_impl("render/vertex_data/background", init);
-
 }
