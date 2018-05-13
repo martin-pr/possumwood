ddb43a305ac1a1e8c59ef862f0a51de670efe822
Martin <martin.prazak@gmail.com>
2018-05-13 17:47:21 +0100

    Recursive loading and saving of Network instances + tests

--- src/libs/dependency_graph/io/graph.cpp
+++ src/libs/dependency_graph/io/graph.cpp
@@ -1,6 +1,7 @@
 #include "graph.h"
 
 #include "node.h"
+#include "node_base.inl"
 #include "io.h"
 #include "../metadata_register.h"
 
@@ -18,6 +19,9 @@ const T& dereference(const T& n) {
 	return n;
 }
 
+void writeNetwork(json& j, const ::dependency_graph::Network& net, const Selection& selection = Selection());
+
+/// a wrapper of node writing, with CONTAINER either a Nodes instance or a Selection
 template<typename CONTAINER>
 void writeNodes(json& j, const CONTAINER& nodes, std::map<std::string, unsigned>& uniqueIds, std::map<const ::dependency_graph::NodeBase*, std::string>& nodeIds) {
 	for(auto& ni : nodes) {
@@ -33,25 +37,26 @@ void writeNodes(json& j, const CONTAINER& nodes, std::map<std::string, unsigned>
 		// and use this to save the node
 		j[name] = n;
 
+		if(n.is<Network>())
+			writeNetwork(j[name], n.as<Network>());
+
 		// remember the assigned ID for connection saving
 		nodeIds[&n] = name;
 	}
 }
 
-}
-
-void to_json(json& j, const ::dependency_graph::Graph& g, const Selection& selection) {
+void writeNetwork(json& j, const ::dependency_graph::Network& net, const Selection& selection) {
 	std::map<std::string, unsigned> uniqueIds;
 	std::map<const ::dependency_graph::NodeBase*, std::string> nodeIds;
 
 	j["nodes"] = "{}"_json;
 	if(selection.empty())
-		writeNodes(j["nodes"], g.nodes(), uniqueIds, nodeIds);
+		writeNodes(j["nodes"], net.nodes(), uniqueIds, nodeIds);
 	else
 		writeNodes(j["nodes"], selection.nodes(), uniqueIds, nodeIds);
 
 	j["connections"] = "[]"_json;
-	for(auto& c : g.connections()) {
+	for(auto& c : net.connections()) {
 		auto itOut = nodeIds.find(&c.first.node());
 		auto itIn = nodeIds.find(&c.second.node());
 		if(itOut != nodeIds.end() && itIn != nodeIds.end()) {
@@ -67,18 +72,27 @@ void to_json(json& j, const ::dependency_graph::Graph& g, const Selection& selec
 	}
 }
 
+}
+
+void to_json(json& j, const ::dependency_graph::Graph& g, const Selection& selection) {
+	writeNetwork(j, g, selection);
+}
+
 void adl_serializer<Graph>::to_json(json& j, const ::dependency_graph::Graph& g) {
 	::dependency_graph::io::to_json(j, g);
 }
 
 //////////////
 
-void from_json(const json& j, Graph& g) {
+namespace {
+
+void loadNetwork(const json& j, Network& net) {
 	std::map<std::string, dependency_graph::UniqueId> nodeIds;
 
 	for(json::const_iterator ni = j["nodes"].begin(); ni != j["nodes"].end(); ++ni) {
 		const json& n = ni.value();
 
+		// extract the blind data via factory mechanism
 		std::unique_ptr<BaseData> blindData;
 		if(!n["blind_data"].is_null()) {
 			blindData = BaseData::create(n["blind_data"]["type"].get<std::string>());
@@ -87,18 +101,25 @@ void from_json(const json& j, Graph& g) {
 			io::fromJson(n["blind_data"]["value"], *blindData);
 		}
 
+		// find the metadata instance
 		const MetadataHandle& meta = MetadataRegister::singleton()[n["type"].get<std::string>()];
-		NodeBase& node = g.nodes().add(meta, n["name"].get<std::string>(), std::move(blindData));
+		NodeBase& node = net.nodes().add(meta, n["name"].get<std::string>(), std::move(blindData));
 
+		// read the node data
 		adl_serializer<NodeBase>::from_json(n, node);
 
+		// special handling for networks - load the network on top of base node data
+		if(node.is<Network>())
+			loadNetwork(ni.value(), node.as<Network>());
+
+		// and record the new node instance, in index-to-uniqueid translation
 		assert(nodeIds.find(ni.key()) == nodeIds.end());
 		nodeIds[ni.key()] = node.index();
 	}
 
 	for(auto& c : j["connections"]) {
-		NodeBase& n1 = g.nodes()[nodeIds[c["out_node"].get<std::string>()]];
-		NodeBase& n2 = g.nodes()[nodeIds[c["in_node"].get<std::string>()]];
+		NodeBase& n1 = net.nodes()[nodeIds[c["out_node"].get<std::string>()]];
+		NodeBase& n2 = net.nodes()[nodeIds[c["in_node"].get<std::string>()]];
 
 		int p1 = -1;
 		for(unsigned p=0;p<n1.portCount();++p)
@@ -113,6 +134,12 @@ void from_json(const json& j, Graph& g) {
 		if(p1 >= 0 && p2 >= 0)
 			n1.port(p1).connect(n2.port(p2));
 	}
+}
+
+}
+
+void from_json(const json& j, Graph& g) {
+	loadNetwork(j, g);
 }
 
 void adl_serializer<Graph>::from_json(const json& j, ::dependency_graph::Graph& g) {
--- src/tests/dependency_graph/json.cpp
+++ src/tests/dependency_graph/json.cpp
@@ -6,12 +6,13 @@
 #include <dependency_graph/io/graph.h>
 #include <dependency_graph/port.inl>
 #include <dependency_graph/rtti.h>
+#include <dependency_graph/metadata_register.h>
 
 #include "common.h"
 
 using namespace dependency_graph;
 
-BOOST_AUTO_TEST_CASE(json_io) {
+BOOST_AUTO_TEST_CASE(simple_graph_saving) {
 	Graph g;
 
 	// empty serialization
@@ -118,6 +119,191 @@ BOOST_AUTO_TEST_CASE(json_io) {
 							{"out_port", "output"}
 						}
 					}
+				}
+			}
+		);
+
+
+		m.port(0).set<float>(3.0f);
+		m.port(1).set<float>(5.0f);
+
+		a.port(2).connect(m.port(0));
+
+		m.setBlindData<std::string>("test blind data");
+
+
+		io::json json;
+		BOOST_REQUIRE_NO_THROW(json = g);
+		BOOST_CHECK_EQUAL(json, result);
+
+
+		Graph g2;
+		dependency_graph::io::adl_serializer<Graph>::from_json(json, g2);
+
+		io::json json2;
+		BOOST_REQUIRE_NO_THROW(json2 = g2);
+		BOOST_CHECK_EQUAL(json2, result);
+	}
+}
+
+
+BOOST_AUTO_TEST_CASE(nested_graph_saving) {
+	Graph g;
+
+	Network* net = nullptr;
+
+	// empty network serialization
+	{
+		// add an empty network
+		{
+			MetadataHandle networkMeta = MetadataRegister::singleton()["network"];
+			NodeBase& base = g.nodes().add(networkMeta, "test_network");
+
+			BOOST_REQUIRE(base.is<Network>());
+			net = &(base.as<Network>());
+		}
+
+		const io::json result(
+			{
+				{
+					"nodes", {
+						{"network_0", {
+							{"name", "test_network"},
+							{"type", "network"},
+							// {"ports", {}},
+							{"blind_data", nullptr},
+							{"nodes", "{}"_json},
+							{"connections", "[]"_json}
+						}}
+					},
+				},
+				{
+					"connections", "[]"_json
+				}
+			}
+		);
+
+		io::json json;
+
+		BOOST_REQUIRE_NO_THROW(json = g);
+		BOOST_CHECK_EQUAL(json, result);
+
+		Graph g2;
+		dependency_graph::io::adl_serializer<Graph>::from_json(json, g2);
+
+		io::json json2;
+		BOOST_REQUIRE_NO_THROW(json2 = g2);
+		BOOST_CHECK_EQUAL(json2, result);
+	}
+
+	// a single node, no connections, no blind data
+	NodeBase& a = net->nodes().add(additionNode(), "add");
+	{
+		const io::json result(
+			{
+				{
+					"nodes", {
+						{"network_0", {
+							{"name", "test_network"},
+							{"type", "network"},
+							// {"ports", {}},
+							{"blind_data", nullptr},
+							{"nodes", {
+								{"addition_0", {
+									{"name", "add"},
+									{"type", "addition"},
+									{"ports", {
+										{"input_1", 2.0},
+										{"input_2", 4.0}}},
+									{"blind_data", nullptr}
+								}}
+							}},
+							{"connections", "[]"_json}
+						}}
+					},
+				},
+				{
+					"connections", "[]"_json
+				}
+			}
+		);
+
+		a.port(0).set<float>(2.0f);
+		a.port(1).set<float>(4.0f);
+
+		// test saving
+		io::json json;
+		BOOST_REQUIRE_NO_THROW(json = g);
+		BOOST_CHECK_EQUAL(json, result);
+
+		// and test that it can be loaded again
+		Graph g2;
+		dependency_graph::io::adl_serializer<Graph>::from_json(json, g2);
+
+		io::json json2;
+		BOOST_REQUIRE_NO_THROW(json2 = g2);
+		BOOST_CHECK_EQUAL(json2, result);
+	}
+
+	// three nodes, a connection, blind data
+	NodeBase& m = net->nodes().add(multiplicationNode(), "mult");
+	net->nodes().add(multiplicationNode(), "mult");
+	{
+		const io::json result(
+			{
+				{
+					"nodes", {
+						{"network_0", {
+							{"name", "test_network"},
+							{"type", "network"},
+							// {"ports", {}},
+							{"blind_data", nullptr},
+							{"nodes", {
+								{"addition_0", {
+									{"name", "add"},
+									{"type", "addition"},
+									{"ports", {
+										{"input_1", 2.0},
+										{"input_2", 4.0}
+									}},
+									{"blind_data", nullptr}
+								}},
+								{"multiplication_0", {
+									{"name", "mult"},
+									{"type", "multiplication"},
+									{"ports", {
+										{"input_2", 5.0}
+									}},
+									{"blind_data", {
+										{"type", unmangledTypeId<std::string>()},
+										{"value", "test blind data"}
+									}}
+								}},
+								{"multiplication_1", {
+									{"name", "mult"},
+									{"type", "multiplication"},
+									{"ports", {
+										{"input_1", 0.0},
+										{"input_2", 0.0}
+									}},
+									{"blind_data", nullptr}
+								}}
+							}},
+							{
+								"connections", {
+									{
+										{"in_node", "multiplication_0"},
+										{"in_port", "input_1"},
+										{"out_node", "addition_0"},
+										{"out_port", "output"}
+									}
+								}
+							}
+						}}
+					},
+				},
+				{
+					"connections", "[]"_json
 				}
 			}
 		);
