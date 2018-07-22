9046017a960e97291b75d30caf6a25adb12026ca
Martin <martin.prazak@gmail.com>
2018-07-22 15:43:33 +0100

    Added setValueAction that works with port name and a json value - required for loading a port value

--- src/libs/actions/detail/values.h
+++ src/libs/actions/detail/values.h
@@ -4,6 +4,8 @@
 
 #include <dependency_graph/network.h>
 
+#include <actions/io/json.h>
+
 #include "../undo_stack.h"
 #include "../node_data.h"
 
@@ -11,5 +13,7 @@ namespace possumwood { namespace actions { namespace detail {
 
 possumwood::UndoStack::Action setValueAction(dependency_graph::Port& port, const dependency_graph::BaseData& value);
 possumwood::UndoStack::Action setValueAction(const dependency_graph::UniqueId& nodeId, std::size_t portId, const dependency_graph::BaseData& value);
+
+possumwood::UndoStack::Action setValueAction(const dependency_graph::UniqueId& nodeId, const std::string& portName, const possumwood::io::json& value);
 
 } } }
