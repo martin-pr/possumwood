204408401ec4a94605b16d6ae96f74c68db6b843
Martin <martin.prazak@gmail.com>
2019-01-05 17:28:09 +0000

    Added very simple program options parsing and scene loading to the CLI

--- src/apps/possumwood_cli/main.cpp
+++ src/apps/possumwood_cli/main.cpp
@@ -3,6 +3,7 @@
 #include <possumwood_sdk/app.h>
 
 #include "common.h"
+#include "options.h"
 
 int main(int argc, char* argv[]) {
 	// create the possumwood application
@@ -10,6 +11,23 @@ int main(int argc, char* argv[]) {
 
 	// load all plugins into an RAII container
 	PluginsRAII plugins;
+
+	// parse the program options
+	Options options(argc, argv);
+
+	for(auto& option : options) {
+		// scene loading
+		if(option.name == "--scene") {
+			if(option.parameters.size() != 1)
+				throw std::runtime_error("--scene option allows only exactly one filename");
+
+			std::cout << "Loading " << option.parameters[0] << "... " << std::flush;
+			papp.loadFile(boost::filesystem::path(option.parameters[0]));
+			std::cout << "done" << std::endl;
+		}
+		else
+			throw std::runtime_error("Unknown command line option " + option.name);
+	}
 
 	return 0;
 }
---
+++ src/apps/possumwood_cli/options.cpp
@@ -0,0 +1,26 @@
+#include "options.h"
+
+#include <iostream>
+
+#include <boost/algorithm/string/predicate.hpp>
+
+Options::Options(int argc, char* argv[]) {
+	for(int current=1; current<argc; ++current) {
+		if(boost::starts_with(argv[current], "--"))
+			m_items.push_back({{argv[current]}});
+
+		else if(!m_items.empty())
+			m_items.back().parameters.push_back(argv[current]);
+
+		else
+			throw std::runtime_error("Command line parameters must start with '--'");
+	}
+}
+
+Options::const_iterator Options::begin() const {
+	return m_items.begin();
+}
+
+Options::const_iterator Options::end() const {
+	return m_items.end();
+}
