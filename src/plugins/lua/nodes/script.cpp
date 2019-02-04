0921796ad4b4ddd40fb7e71ff51b37e2e6335762
Martin <martin.prazak@gmail.com>
2019-02-04 09:05:54 +0000

    Added a simple Lua code editor to lua/script node

--- src/plugins/lua/nodes/script.cpp
+++ src/plugins/lua/nodes/script.cpp
@@ -3,6 +3,11 @@
 #include <memory>
 
 #include <possumwood_sdk/node_implementation.h>
+#include <possumwood_sdk/source_editor.h>
+
+#include <QPainter>
+#include <QPushButton>
+#include <QMenu>
 
 #include <luabind/luabind.hpp>
 
@@ -11,14 +16,73 @@ namespace {
 dependency_graph::InAttr<std::string> a_src;
 dependency_graph::OutAttr<float> a_out;
 
+class Popup : public QMenu {
+	public:
+		Popup(QWidget* parent) : QMenu(parent) {
+		}
+
+		void addItem(const std::string& name) {
+			addAction(name.c_str());
+		}
+
+	private:
+};
+
+
+class Editor : public possumwood::SourceEditor {
+	public:
+		Editor() : SourceEditor(a_src), m_popup(nullptr) {
+			m_varsButton = new QPushButton("Variables");
+			buttonsLayout()->insertWidget(0, m_varsButton);
+
+			widget()->connect(m_varsButton, &QPushButton::pressed, [this]() {
+				if(m_popup)
+					m_popup->popup(
+						m_varsButton->mapToGlobal(QPoint(0,-m_popup->sizeHint().height()))
+					);
+			});
+		}
+
+	protected:
+		virtual void valueChanged(const dependency_graph::Attr& attr) override {
+				SourceEditor::valueChanged(attr);
+		}
+
+		void populateVariableList() {
+			if(!m_popup)
+				m_popup->deleteLater();
+
+			m_popup = new Popup(m_varsButton);
+
+			m_popup->connect(m_popup, &Popup::triggered, [this](QAction* action) {
+				QString text = action->text();
+				while(!text.isEmpty() && !QChar(text[0]).isLetterOrNumber())
+					text = text.mid(1, text.length()-1);
+				if(text.indexOf('\t') >= 0)
+					text = text.mid(0, text.indexOf('\t'));
+
+				editorWidget()->insertPlainText(text);
+			});
+		}
+
+	private:
+		QPushButton* m_varsButton;
+
+		Popup* m_popup;
+};
+
 dependency_graph::State compute(dependency_graph::Values& data) {
 	dependency_graph::State result;
 
 	const std::string& src = data.get(a_src);
 
+	// Create a new lua state
 	lua_State *myLuaState = luaL_newstate();
 
+	// Connect LuaBind to this lua state
 	luabind::open(myLuaState);
+
+	// Define a lua function that we can call
 	luaL_dostring(myLuaState, src.c_str());
 
 	const float out = luabind::call_function<float>(myLuaState, "main");
@@ -36,6 +100,7 @@ void init(possumwood::Metadata& meta) {
 	meta.addInfluence(a_src, a_out);
 
 	meta.setCompute(&compute);
+	meta.setEditor<Editor>();
 }
 
 possumwood::NodeImplementation s_impl("lua/script", init);
