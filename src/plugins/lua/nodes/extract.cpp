078b8498e51e24e7fee280fed34b0344f3639a86
Martin <martin.prazak@gmail.com>
2019-03-03 19:19:31 +0000

    Generalized implementation of lua/extract/* nodes - registering a new node/type is now just a single specialisation line

---
+++ src/plugins/lua/nodes/extract.cpp
@@ -0,0 +1,11 @@
+#include "extract.h"
+
+namespace {
+
+possumwood::NodeImplementation s_impl_unsigned("lua/extract/unsigned",
+	possumwood::lua::Extract<unsigned>::init);
+
+possumwood::NodeImplementation s_impl_float("lua/extract/float",
+	possumwood::lua::Extract<float>::init);
+
+}
---
+++ src/plugins/lua/nodes/extract.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <possumwood_sdk/node_implementation.h>
+
+#include "datatypes/context.h"
+#include "datatypes/variable.inl"
+
+namespace possumwood { namespace lua {
+
+template<typename T>
+struct Extract {
+	struct Params {
+		dependency_graph::InAttr<std::string> a_name;
+		dependency_graph::InAttr<std::shared_ptr<const possumwood::lua::State>> a_state;
+		dependency_graph::OutAttr<T> a_out;
+	};
+
+	static dependency_graph::State compute(dependency_graph::Values& data, Params& params) {
+		std::shared_ptr<const possumwood::lua::State> state = data.get(params.a_state);
+
+		if(!state)
+			throw std::runtime_error("Uninitialised state - cannot extract a value.");
+
+		try {
+			// get the result
+			T value = luabind::object_cast<T>(state->globals()[data.get(params.a_name)]);
+			// and push it to the output
+			data.set(params.a_out, value);
+		}
+		catch(const luabind::error& err) {
+			throw std::runtime_error(lua_tostring(err.state(), -1));
+		}
+
+		return dependency_graph::State();
+	}
+
+	static void init(possumwood::Metadata& meta) {
+		static Params s_params;
+
+		meta.addAttribute(s_params.a_name, "name", std::string("variable"));
+		meta.addAttribute(s_params.a_state, "state");
+
+		meta.addAttribute(s_params.a_out, "out");
+
+		meta.addInfluence(s_params.a_name, s_params.a_out);
+		meta.addInfluence(s_params.a_state, s_params.a_out);
+
+		meta.setCompute([&](dependency_graph::Values& data) {
+			return compute(data, s_params);
+		});
+	}
+};
+
+} }
