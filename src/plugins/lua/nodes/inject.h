6f66ee5e605a9766189f2f49810b9b9f0eaa4586
Martin <martin.prazak@gmail.com>
2019-03-03 19:29:06 +0000

    Generalized implementation of lua/inject/* nodes - registering a new node/type is now just a single specialisation line

---
+++ src/plugins/lua/nodes/inject.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <possumwood_sdk/node_implementation.h>
+
+#include "datatypes/context.h"
+#include "datatypes/variable.inl"
+
+namespace possumwood { namespace lua {
+
+template<typename T>
+struct Inject {
+	struct Params {
+		dependency_graph::InAttr<std::string> a_name;
+		dependency_graph::InAttr<T> a_value;
+		dependency_graph::InAttr<possumwood::lua::Context> a_inContext;
+		dependency_graph::OutAttr<possumwood::lua::Context> a_outContext;
+	};
+
+	static dependency_graph::State compute(dependency_graph::Values& data, const Params& params) {
+		possumwood::lua::Context context = data.get(params.a_inContext);
+		context.addVariable(possumwood::lua::Variable(data.get(params.a_name), data.get(params.a_value)));
+		data.set(params.a_outContext, context);
+
+		return dependency_graph::State();
+	}
+
+	static void init(possumwood::Metadata& meta) {
+		static Params s_params;
+
+		meta.addAttribute(s_params.a_name, "name", std::string("constant"));
+		meta.addAttribute(s_params.a_value, "value", T());
+		meta.addAttribute(s_params.a_inContext, "in_context");
+
+		meta.addAttribute(s_params.a_outContext, "out_context");
+
+		meta.addInfluence(s_params.a_name, s_params.a_outContext);
+		meta.addInfluence(s_params.a_value, s_params.a_outContext);
+		meta.addInfluence(s_params.a_inContext, s_params.a_outContext);
+
+		meta.setCompute([&](dependency_graph::Values& data) {
+			return compute(data, s_params);
+		});
+	}
+};
+
+} }
