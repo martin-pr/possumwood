206d63171a9e6926a1b1b0790443f51c8ece87f1
Martin <martin.prazak@gmail.com>
2019-03-26 23:35:18 +0000

    Type registration for serialization now based on std::type_index instances, to avoid destruction order problems

--- src/libs/actions/io.cpp
+++ src/libs/actions/io.cpp
@@ -1,22 +1,22 @@
 #include "io.h"
 
 namespace {
-	std::map<std::string, possumwood::IOBase::to_fn>& s_toFn() {
-		static std::map<std::string, possumwood::IOBase::to_fn> s_map;
+	std::map<std::type_index, possumwood::IOBase::to_fn>& s_toFn() {
+		static std::map<std::type_index, possumwood::IOBase::to_fn> s_map;
 		return s_map;
 	}
 
-	std::map<std::string, possumwood::IOBase::from_fn>& s_fromFn() {
-		static std::map<std::string, possumwood::IOBase::from_fn> s_map;
+	std::map<std::type_index, possumwood::IOBase::from_fn>& s_fromFn() {
+		static std::map<std::type_index, possumwood::IOBase::from_fn> s_map;
 		return s_map;
 	}
 }
 
 namespace possumwood {
 
-IOBase::IOBase(const std::string& type, to_fn toJson, from_fn fromJson) : m_type(type) {
-	assert(s_toFn().find(type) == s_toFn().end() && "only one serialization per data type should be implemented");
-	assert(s_fromFn().find(type) == s_fromFn().end() && "only one serialization per data type should be implemented");
+IOBase::IOBase(const std::type_index& type, to_fn toJson, from_fn fromJson) : m_type(type) {
+	// assert(s_toFn().find(type) == s_toFn().end() && "only one serialization per data type should be implemented");
+	// assert(s_fromFn().find(type) == s_fromFn().end() && "only one serialization per data type should be implemented");
 
 	s_toFn()[type] = toJson;
 	s_fromFn()[type] = fromJson;
@@ -39,7 +39,7 @@ IOBase::~IOBase() {
 namespace possumwood { namespace io {
 
 void fromJson(const json& j, dependency_graph::BaseData& data) {
-	auto it = s_fromFn().find(data.type());
+	auto it = s_fromFn().find(data.typeinfo());
 
 	#ifndef NDEBUG
 	if(it == s_fromFn().end())
@@ -51,7 +51,7 @@ void fromJson(const json& j, dependency_graph::BaseData& data) {
 }
 
 void toJson(json& j, const dependency_graph::BaseData& data) {
-	auto it = s_toFn().find(data.type());
+	auto it = s_toFn().find(data.typeinfo());
 	#ifndef NDEBUG
 	if(it == s_toFn().end())
 		std::cout << "Error - no json serialization implemented for type " << data.type() << std::endl;
@@ -68,7 +68,7 @@ namespace dependency_graph { namespace io {
 struct SaveableRegistration {
 	SaveableRegistration() {
 		setIsSaveableCallback([](const BaseData& data) {
-			return s_toFn().find(data.type()) != s_toFn().end();
+			return s_toFn().find(data.typeinfo()) != s_toFn().end();
 		});
 	}
 
