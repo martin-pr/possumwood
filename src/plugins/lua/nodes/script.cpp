dd7e0a455767df954524980a3704c140e274dfa3
Martin <martin.prazak@gmail.com>
2019-04-18 08:56:14 +0100

    Recursively evaluated menu of global symbols, with classes, instances and methods

--- src/plugins/lua/nodes/script.cpp
+++ src/plugins/lua/nodes/script.cpp
@@ -7,7 +7,9 @@
 #include <QPushButton>
 #include <QMenu>
 
+#include <lualib.h>
 #include <luabind/luabind.hpp>
+#include <luabind/class_info.hpp>
 
 #include "datatypes/state.h"
 #include "datatypes/context.h"
@@ -34,7 +36,7 @@ class Popup : public QMenu {
 class Editor : public possumwood::SourceEditor {
 	public:
 		Editor() : SourceEditor(a_src), m_popup(nullptr) {
-			m_varsButton = new QPushButton("Variables");
+			m_varsButton = new QPushButton("Globals");
 			buttonsLayout()->insertWidget(0, m_varsButton);
 
 			widget()->connect(m_varsButton, &QPushButton::pressed, [this]() {
@@ -47,7 +49,7 @@ class Editor : public possumwood::SourceEditor {
 
 	protected:
 		virtual void valueChanged(const dependency_graph::Attr& attr) override {
-			if(attr == a_context) {
+			if(attr == a_state) {
 				if(m_popup)
 					m_popup->deleteLater();
 
@@ -58,19 +60,26 @@ class Editor : public possumwood::SourceEditor {
 				SourceEditor::valueChanged(attr);
 		}
 
-		std::string luaType(int type) {
-			switch(type) {
-				case LUA_TNONE: return "LUA_TNONE";
+		std::string luaType(const luabind::object& o) {
+			int type = luabind::type(o);
 
-				case LUA_TNIL: return "LUA_TNIL";
-				case LUA_TBOOLEAN: return "LUA_TBOOLEAN";
-				case LUA_TLIGHTUSERDATA: return "LUA_TLIGHTUSERDATA";
-				case LUA_TNUMBER: return "LUA_TNUMBER";
-				case LUA_TSTRING: return "LUA_TSTRING";
-				case LUA_TTABLE: return "LUA_TTABLE";
-				case LUA_TFUNCTION: return "LUA_TFUNCTION";
-				case LUA_TUSERDATA: return "LUA_TUSERDATA";
-				case LUA_TTHREAD: return "LUA_TTHREAD";
+			switch(type) {
+				case LUA_TNONE: return "none";
+				case LUA_TNIL: return "nil";
+				case LUA_TBOOLEAN: return "bool";
+				case LUA_TLIGHTUSERDATA: return "light object";
+				case LUA_TNUMBER: return "number";
+				case LUA_TSTRING: return "string";
+				case LUA_TTABLE: return "table";
+				case LUA_TFUNCTION: return "function";
+				case LUA_TTHREAD: return "thread";
+				case LUA_TUSERDATA:
+					if(isClassInstance(o))
+						return "object";
+					else if(isClass(o))
+						return "class";
+					else
+						return "lua class";
 			}
 
 			return "unknown";
@@ -79,70 +88,116 @@ class Editor : public possumwood::SourceEditor {
 		// add one item to the menu
 		// TODO: extend to add values as well
 		void addItem(QMenu* menu, const luabind::object& o, const std::string& name) {
-			menu->addAction((name + "\t" + luaType(luabind::type(o))).c_str());
+			menu->addAction((name + "\t" + luaType(o)).c_str());
 		}
 
 		// add a submenu to the popup
 		QMenu* addMenu(QMenu* menu, const luabind::object& o, const std::string& name) {
-			return menu->addMenu((name + "\t" + luaType(luabind::type(o))).c_str());
+			return menu->addMenu((name + "\t" + luaType(o)).c_str());
 		}
 
-		// parses the metadata table (non-recursively, to avoid infinite recursion, as all tables have a meta table)
-		void parseMeta(QMenu* menu, const luabind::object& o) {
+		void parseClass(QMenu* menu, const luabind::object& o, int recurse) {
+			o.push(o.interpreter());
+			luabind::detail::object_rep* obj = luabind::detail::get_instance(o.interpreter(), -1);
+			if(obj && obj->crep()) {
+				obj->crep()->get_table(o.interpreter());
+				luabind::object ooo(luabind::from_stack(o.interpreter(), -1));
+
+				if(recurse > 0)
+					parseGlobals(menu, ooo, recurse-1);
+
+				lua_settop(o.interpreter(), 0);
+			}
+		}
+
+		bool isClass(const luabind::object& o) {
 			if(o.is_valid()) {
 				auto meta = luabind::getmetatable(o);
 
-				if(meta.is_valid() && luabind::type(meta) == LUA_TTABLE)
-					parseGlobals(menu, meta, false);
+				if(meta.is_valid() && luabind::type(meta) == LUA_TTABLE) {
+					for(luabind::iterator j(meta); j != luabind::iterator(); ++j) {
+						auto key = luabind::object_cast_nothrow<std::string>(j.key());
+						if(key && *key == "__luabind_classrep")
+							return true;
+					}
+				}
+			}
 
-				if(meta["__luabind_classrep"].operator luabind::object().is_valid()) {
-					// auto ci = luabind::get_class_info(o);
+			return false;
+		}
 
-					addItem(menu, meta, "metadata");
+		bool isClassInstance(const luabind::object& o) {
+			auto meta = luabind::getmetatable(o);
+
+			if(luabind::type(meta) == LUA_TTABLE) {
+				for(luabind::iterator j(meta); j != luabind::iterator(); ++j) {
+					auto key = luabind::object_cast_nothrow<std::string>(j.key());
+					if(key && *key == "__luabind_class")
+						return true;
+				}
+			}
+
+			return false;
+		}
+
+		// parses the metadata table
+		void parseMeta(QMenu* menu, const luabind::object& o, int recurse = 10) {
+			if(o.is_valid()) {
+				assert(!isClass(o));
+
+				// adds all members for this instance
+				if(isClassInstance(o))
+					parseClass(menu, o, recurse-1);
+
+				// just descends recursively
+				else {
+					auto meta = luabind::getmetatable(o);
+
+					if(meta.is_valid() && luabind::type(meta) == LUA_TTABLE)
+						parseGlobals(menu, meta, recurse-1);
 				}
 			}
 		}
 
 		// parse a table and add its content to the menu - first explicitly on globals, then recurse
-		void parseGlobals(QMenu* menu, const luabind::object& o, bool recurse = true) {
+		void parseGlobals(QMenu* menu, const luabind::object& o, int recurse = 10) {
 			assert(o.is_valid());
 			assert(luabind::type(o) == LUA_TTABLE);
 
 			// iterate over the items of this table
-			for(luabind::iterator j(o); j != luabind::iterator(); ++j)
-				if((*j).operator luabind::object().is_valid()) {
+			for(luabind::iterator j(o), end; j != end; ++j) {
+				luabind::object obj = *j;
 
+				if(obj.is_valid()) {
 					// try to obtain a key as a string - this might be a number of an array, so treat that appropriately
-					std::string key;
+					auto key = luabind::object_cast_nothrow<std::string>(j.key());
+					if(key) {
+						// the current object is a table - add a submenu and proceed recursively
+						if(luabind::type(obj) == LUA_TTABLE) {
+							QMenu* m = addMenu(menu, obj, *key);
 
-					auto s = luabind::object_cast_nothrow<std::string>(j.key());
-					if(s)
-						key = *s;
+							if(recurse > 0)
+								parseGlobals(m, obj, recurse - 1);
+						}
 
-					auto n = luabind::object_cast_nothrow<double>(j.key());
-					if(n)
-						key = std::to_string(*n);
+						// a luabind class - only show it as a single item
+						else if(luabind::type(obj) == LUA_TUSERDATA && isClass(obj))
+							addItem(menu, obj, *key);
 
-					// the current object is a table - add a submenu and proceed recursively
-					if(luabind::type(*j) == LUA_TTABLE) {
-						QMenu* m = addMenu(menu, *j, key);
+						// a userdata object - either an inbuilt class or a luabind instance
+						else if(luabind::type(obj) == LUA_TUSERDATA) {
+							if(recurse > 0) {
+								QMenu* m = addMenu(menu, obj, *key);
+								parseMeta(m, obj, recurse - 1);
+							}
+						}
 
-						if(recurse)
-							parseGlobals(m, *j);
+						// functions, numbers, strings ... just add them to the menu
+						else
+							addItem(menu, obj, *key);
 					}
-
-					// a userdata object (C++ bound class) - parse its metadata
-					else if(luabind::type(*j) == LUA_TUSERDATA) {
-						QMenu* m = addMenu(menu, *j, key);
-
-						if(recurse)
-							parseMeta(m, *j);
-					}
-
-					// functions, numbers, strings ... just add them to the menu
-					else
-						addItem(menu, *j, key);
 				}
+			}
 		}
 
 		void populateVariableList() {
@@ -161,13 +216,10 @@ class Editor : public possumwood::SourceEditor {
 				editorWidget()->insertPlainText(text);
 			});
 
-			// make a dummy state
-			possumwood::lua::State state(values().get(a_context));
-			// add the class info function - needed to query bound functions
-			luabind::bind_class_info(state);
-
-			// parse the global variables of the state (includes injected vars and modules)
-			parseGlobals(m_popup, state.globals());
+			// parse the global variables of the state from the output (includes injected vars and modules)
+			std::shared_ptr<const possumwood::lua::State> state = values().get(a_state);
+			if(state)
+				parseGlobals(m_popup, state->globals());
 		}
 
 	private:
@@ -185,6 +237,12 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 	std::shared_ptr<possumwood::lua::State> state(new possumwood::lua::State(data.get(a_context)));
 
 	try {
+		// load all standard Lua libraries
+		// luaL_openlibs(*state);
+
+		// luabind class info function - allows introspection of luabind classes
+		luabind::bind_class_info(*state);
+
 		// evaluate our script
 		int err = luaL_dostring(*state, src.c_str());
 		if(err)
