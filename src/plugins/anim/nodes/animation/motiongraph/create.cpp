67a3b23257d1dd870f6888be47b87c54f8b8be0f
Martin <martin.prazak@gmail.com>
2019-05-06 17:07:15 +0100

    A simple implementation of a motion graph, simply connecting snippets of animations generated from a list of transition points. Does not do any blends (yet).

--- src/plugins/anim/nodes/animation/motiongraph/create.cpp
+++ src/plugins/anim/nodes/animation/motiongraph/create.cpp
@@ -13,6 +13,7 @@
 
 #include "datatypes/skeleton.h"
 #include "datatypes/animation.h"
+#include "datatypes/motion_graph.h"
 #include "ui/motion_map.h"
 
 namespace {
@@ -20,6 +21,7 @@ namespace {
 dependency_graph::InAttr<anim::Animation> a_inAnim;
 dependency_graph::InAttr<unsigned> a_transitionCount, a_transitionLength, a_filterSelftransitions;
 dependency_graph::OutAttr<anim::MotionMap> a_mmap;
+dependency_graph::OutAttr<anim::MotionGraph> a_mgraph;
 
 class Editor : public possumwood::Editor {
 	public:
@@ -62,17 +64,23 @@ class Editor : public possumwood::Editor {
 };
 
 dependency_graph::State compute(dependency_graph::Values& values) {
-	::anim::MotionMap mmap(values.get(a_inAnim), ::anim::metric::LocalAngle());
+	// make a motion map
+	if(values.isDirty(a_mmap)) {
+		::anim::MotionMap mmap(values.get(a_inAnim), ::anim::metric::LocalAngle());
 
-	::anim::filter::LinearTransition lin(values.get(a_transitionLength));
-	mmap.filter(lin);
+		::anim::filter::LinearTransition lin(values.get(a_transitionLength));
+		mmap.filter(lin);
 
-	::anim::filter::IgnoreIdentity ident(values.get(a_filterSelftransitions));
-	mmap.filter(ident);
+		::anim::filter::IgnoreIdentity ident(values.get(a_filterSelftransitions));
+		mmap.filter(ident);
 
-	mmap.computeLocalMinima(values.get(a_transitionCount), values.get(a_transitionLength)/2+1);
+		mmap.computeLocalMinima(values.get(a_transitionCount), values.get(a_transitionLength)/2+1);
 
-	values.set(a_mmap, mmap);
+		values.set(a_mmap, mmap);
+	}
+
+	// make a motion graph out of this
+	values.set(a_mgraph, anim::MotionGraph(values.get(a_inAnim), values.get(a_mmap).localMinima()));
 
 	return dependency_graph::State();
 }
@@ -80,20 +88,26 @@ dependency_graph::State compute(dependency_graph::Values& values) {
 void init(possumwood::Metadata& meta) {
 	meta.addAttribute(a_inAnim, "in_anim", anim::Animation(24.0f), possumwood::AttrFlags::kVertical);
 
-	meta.addAttribute(a_mmap, "motion_map", anim::MotionMap(), possumwood::AttrFlags::kHidden);
 	meta.addAttribute(a_transitionCount, "transition_count", 50u);
 	meta.addAttribute(a_transitionLength, "transition_length", 10u);
 	meta.addAttribute(a_filterSelftransitions, "remove_self_transitions", 20u);
+	meta.addAttribute(a_mmap, "motion_map", anim::MotionMap(), possumwood::AttrFlags::kHidden);
+	meta.addAttribute(a_mgraph, "motion_graph", anim::MotionGraph(), possumwood::AttrFlags::kVertical);
 
 	meta.addInfluence(a_inAnim, a_mmap);
 	meta.addInfluence(a_transitionCount, a_mmap);
 	meta.addInfluence(a_transitionLength, a_mmap);
 	meta.addInfluence(a_filterSelftransitions, a_mmap);
 
+	meta.addInfluence(a_inAnim, a_mgraph);
+	meta.addInfluence(a_transitionCount, a_mgraph);
+	meta.addInfluence(a_transitionLength, a_mgraph);
+	meta.addInfluence(a_filterSelftransitions, a_mgraph);
+
 	meta.setEditor<Editor>();
 	meta.setCompute(compute);
 }
 
-possumwood::NodeImplementation s_impl("anim/animation/motiongraph", init);
+possumwood::NodeImplementation s_impl("anim/animation/motiongraph/create", init);
 
 }
