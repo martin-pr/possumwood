7fbdf139937e4eee62a1dd83be1e00286b0f9353
Martin Prazak <martin.prazak@gmail.com>
2019-06-18 07:15:22 +0100

    Subtract + equalise nodes + a simple example of noise extraction

---
+++ src/plugins/opencv/nodes/equalize.cpp
@@ -0,0 +1,36 @@
+#include <possumwood_sdk/node_implementation.h>
+
+#include <opencv2/opencv.hpp>
+
+#include <actions/traits.h>
+
+#include "frame.h"
+#include "tools.h"
+
+namespace {
+
+dependency_graph::InAttr<possumwood::opencv::Frame> a_in;
+dependency_graph::OutAttr<possumwood::opencv::Frame> a_out;
+
+dependency_graph::State compute(dependency_graph::Values& data) {
+	cv::Mat mat;
+
+	cv::equalizeHist(*data.get(a_in), mat);
+
+	data.set(a_out, possumwood::opencv::Frame(mat));
+
+	return dependency_graph::State();
+}
+
+void init(possumwood::Metadata& meta) {
+	meta.addAttribute(a_in, "in_frame");
+	meta.addAttribute(a_out, "out_frame");
+
+	meta.addInfluence(a_in, a_out);
+
+	meta.setCompute(compute);
+}
+
+possumwood::NodeImplementation s_impl("opencv/equalize", init);
+
+}
