64c8393a3e39050b5dc422bc2f8254d062aba31a
Martin Prazak <martin.prazak@gmail.com>
2019-06-22 18:16:25 +0100

    Added a magical "frame" node - returns the current frame as an "unsigned" value

---
+++ src/libs/possumwood_sdk/nodes/frame.cpp
@@ -0,0 +1,24 @@
+#include <possumwood_sdk/node_implementation.h>
+#include <possumwood_sdk/app.h>
+
+namespace {
+
+dependency_graph::OutAttr<unsigned> a_frame;
+
+void init(possumwood::Metadata& meta) {
+	meta.addAttribute(a_frame, "frame");
+
+	meta.setCompute([](dependency_graph::Values& vals) {
+		vals.set(a_frame, static_cast<unsigned>(std::round(
+			possumwood::App::instance().time() * 
+			possumwood::App::instance().sceneConfig()["fps"].as<float>())));
+
+		return dependency_graph::State();
+	});
+}
+
+/// This is a hack - "frame" nodes have a single output, set by a callback in the
+/// possumwood SDK's App object
+possumwood::NodeImplementation s_impl("frame", init);
+
+}
