2fd90c03f99175394f602cb5ce1850b62ee5b7ce
Martin Prazak <martin.prazak@gmail.com>
2019-06-26 06:30:43 +0100

    Added opencv::CalibrationPoints data structure, and calibration_points node to accumulate the results

---
+++ src/plugins/opencv/calibration_points.cpp
@@ -0,0 +1,52 @@
+#include "calibration_points.h"
+
+namespace possumwood { namespace opencv {
+
+void CalibrationPoints::Layer::add(const cv::Vec3f& objectPoint, const cv::Vec2f& cameraPoint) {
+	m_objectPoints.push_back(objectPoint);
+	m_cameraPoints.push_back(cameraPoint);
+}
+
+CalibrationPoints::CalibrationPoints(const cv::Size& imageSize) : m_imageSize(imageSize) {
+}
+
+void CalibrationPoints::addLayer(const Layer& l) {
+	m_cameraPoints.push_back(l.m_cameraPoints);
+	m_objectPoints.push_back(l.m_objectPoints);
+}
+
+const std::vector<std::vector<cv::Vec3f>>& CalibrationPoints::objectPoints() const {
+	return m_objectPoints;
+}
+
+const std::vector<std::vector<cv::Vec2f>>& CalibrationPoints::cameraPoints() const {
+	return m_cameraPoints;
+}
+
+const cv::Size& CalibrationPoints::imageSize() const {
+	return m_imageSize;
+}
+
+bool CalibrationPoints::empty() const {
+	return m_cameraPoints.empty();
+}
+
+std::size_t CalibrationPoints::size() const {
+	return m_cameraPoints.size();
+}
+
+bool CalibrationPoints::operator == (const CalibrationPoints& f) const {
+	return m_objectPoints == f.m_objectPoints && m_cameraPoints == f.m_cameraPoints && m_imageSize == f.m_imageSize;
+}
+
+bool CalibrationPoints::operator != (const CalibrationPoints& f) const {
+	return m_objectPoints != f.m_objectPoints || m_cameraPoints != f.m_cameraPoints || m_imageSize != f.m_imageSize;
+}
+
+std::ostream& operator << (std::ostream& out, const CalibrationPoints& f) {
+	out << "(" << f.size() << " calibration point set(s), image size=" << f.imageSize().width << "x" << f.imageSize().height << ")" << std::endl;
+
+	return out;
+}
+
+} }
---
+++ src/plugins/opencv/nodes/calibration/calibrationPoints.cpp
@@ -0,0 +1,71 @@
+#include <possumwood_sdk/node_implementation.h>
+
+#include <opencv2/opencv.hpp>
+
+#include "frame.h"
+#include "calibration_pattern.h"
+#include "calibration_points.h"
+
+namespace {
+
+dependency_graph::InAttr<possumwood::opencv::CalibrationPattern> a_inObjectPattern, a_inCameraPattern;
+
+dependency_graph::InAttr<possumwood::opencv::CalibrationPoints> a_inPoints;
+dependency_graph::OutAttr<possumwood::opencv::CalibrationPoints> a_outPoints;
+
+dependency_graph::State compute(dependency_graph::Values& data) {
+	possumwood::opencv::CalibrationPoints points = data.get(a_inPoints);
+
+	const cv::Mat& objPattern = *data.get(a_inObjectPattern);
+	const cv::Mat& camPattern = *data.get(a_inCameraPattern);
+
+	if(objPattern.rows != camPattern.rows)
+		throw std::runtime_error("Number of object points doesn't match number of camera points - " + std::to_string(objPattern.rows) + " vs " + std::to_string(camPattern.rows));
+
+	if(objPattern.size() != camPattern.size())
+		throw std::runtime_error("Object and camera pattern sizes don't match!");
+
+	// if empty, initialise
+	if(points.empty())
+		points = possumwood::opencv::CalibrationPoints(data.get(a_inCameraPattern).imageSize());
+
+	// not empty - check for consistency
+	else {
+		if(points.imageSize() != data.get(a_inCameraPattern).imageSize())
+			throw std::runtime_error("Points image size is not the same as camera pattern image size!");
+	}
+
+	// copy all the points
+	possumwood::opencv::CalibrationPoints::Layer layer;
+	for(int r=0;r<objPattern.rows;++r) {
+		cv::Vec3f obj(objPattern.ptr<float>(r)[0], objPattern.ptr<float>(r)[1], 0.0f);
+		cv::Vec2f cam(camPattern.ptr<float>(r)[0], camPattern.ptr<float>(r)[1]);
+
+		layer.add(obj, cam);
+	}
+	points.addLayer(layer);
+
+	std::cout << points << std::endl;
+
+	data.set(a_outPoints, points);
+
+	return dependency_graph::State();
+}
+
+void init(possumwood::Metadata& meta) {
+	meta.addAttribute(a_inCameraPattern, "camera_pattern", possumwood::opencv::CalibrationPattern(), possumwood::AttrFlags::kVertical);
+	meta.addAttribute(a_inObjectPattern, "object_pattern", possumwood::opencv::CalibrationPattern(), possumwood::AttrFlags::kVertical);
+
+	meta.addAttribute(a_inPoints, "in_points");
+	meta.addAttribute(a_outPoints, "out_points");
+
+	meta.addInfluence(a_inCameraPattern, a_outPoints);
+	meta.addInfluence(a_inObjectPattern, a_outPoints);
+	meta.addInfluence(a_inPoints, a_outPoints);
+
+	meta.setCompute(compute);
+}
+
+possumwood::NodeImplementation s_impl("opencv/calibration/calibration_points", init);
+
+}
