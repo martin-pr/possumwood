3f445a369fa95f29c33d95691223575c4005e42a
Martin Prazak <martin.prazak@gmail.com>
2019-08-21 23:48:43 +0100

    Simple OpenCV circle drawing

---
+++ src/plugins/opencv/nodes/draw/circle.cpp
@@ -0,0 +1,56 @@
+#include <possumwood_sdk/node_implementation.h>
+
+#include <opencv2/opencv.hpp>
+
+#include <possumwood_sdk/datatypes/enum.h>
+#include <actions/traits.h>
+
+#include "frame.h"
+
+namespace {
+
+dependency_graph::InAttr<possumwood::opencv::Frame> a_inFrame;
+dependency_graph::InAttr<float> a_centerX, a_centerY, a_colorR, a_colorG, a_colorB;
+dependency_graph::InAttr<unsigned> a_radius, a_thickness;
+dependency_graph::InAttr<bool> a_fill;
+dependency_graph::OutAttr<possumwood::opencv::Frame> a_outFrame;
+
+dependency_graph::State compute(dependency_graph::Values& data) {
+	cv::Mat result = (*data.get(a_inFrame)).clone();
+	cv::circle(result, cv::Point(data.get(a_centerX), data.get(a_centerY)), data.get(a_radius),
+		cv::Scalar(data.get(a_colorB), data.get(a_colorG), data.get(a_colorR)),
+		data.get(a_fill) ? -1 : data.get(a_thickness));
+
+	data.set(a_outFrame, possumwood::opencv::Frame(result));
+
+	return dependency_graph::State();
+}
+
+void init(possumwood::Metadata& meta) {
+	meta.addAttribute(a_inFrame, "in_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
+	meta.addAttribute(a_centerX, "center/x", 0.0f);
+	meta.addAttribute(a_centerY, "center/y", 0.0f);
+	meta.addAttribute(a_colorR, "color/r", 127.0f);
+	meta.addAttribute(a_colorG, "color/g", 127.0f);
+	meta.addAttribute(a_colorB, "color/b", 127.0f);
+	meta.addAttribute(a_radius, "radius", 10u);
+	meta.addAttribute(a_thickness, "thickness", 2u);
+	meta.addAttribute(a_fill, "fill", false);
+	meta.addAttribute(a_outFrame, "out_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
+
+	meta.addInfluence(a_inFrame, a_outFrame);
+	meta.addInfluence(a_centerX, a_outFrame);
+	meta.addInfluence(a_centerY, a_outFrame);
+	meta.addInfluence(a_colorR, a_outFrame);
+	meta.addInfluence(a_colorG, a_outFrame);
+	meta.addInfluence(a_colorB, a_outFrame);
+	meta.addInfluence(a_radius, a_outFrame);
+	meta.addInfluence(a_thickness, a_outFrame);
+	meta.addInfluence(a_fill, a_outFrame);
+
+	meta.setCompute(compute);
+}
+
+possumwood::NodeImplementation s_impl("opencv/draw/circle", init);
+
+}
