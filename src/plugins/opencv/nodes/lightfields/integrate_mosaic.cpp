870f24ac478cbc66d7d49eb624601e3d0a610ab1
Martin Prazak <martin.prazak@gmail.com>
2019-09-01 10:52:55 +0100

    Mosaic and nearest lightfield integrations now support additional "norm" output (number of samples per pixel of target) + demo
    # Conflicts:
    #	src/plugins/opencv/nodes/lightfields/integrate_mosaic.cpp
    #	src/plugins/opencv/nodes/lightfields/integrate_nearest.cpp

--- src/plugins/opencv/nodes/lightfields/integrate_mosaic.cpp
+++ src/plugins/opencv/nodes/lightfields/integrate_mosaic.cpp
@@ -17,22 +17,22 @@ dependency_graph::InAttr<possumwood::opencv::Frame> a_in;
 dependency_graph::InAttr<lightfields::Pattern> a_pattern;
 dependency_graph::InAttr<Imath::Vec2<unsigned>> a_size;
 dependency_graph::InAttr<unsigned> a_elements;
-dependency_graph::OutAttr<possumwood::opencv::Frame> a_out;
+dependency_graph::OutAttr<possumwood::opencv::Frame> a_out, a_norm;
 
-void add1channel(float* color, float* n, int colorIndex, const float* value) {
+void add1channel(float* color, uint16_t* n, int colorIndex, const float* value) {
 	color[colorIndex] += value[0];
-	n[colorIndex] += 1.0f;
+	n[colorIndex] += 1;
 }
 
-void add3channel(float* color, float* n, int colorIndex, const float* value) {
+void add3channel(float* color, uint16_t* n, int colorIndex, const float* value) {
 	color[0] += value[0];
-	n[0] += 1.0f;
+	n[0] += 1;
 
 	color[1] += value[1];
-	n[1] += 1.0f;
+	n[1] += 1;
 
 	color[2] += value[2];
-	n[2] += 1.0f;
+	n[2] += 1;
 }
 
 dependency_graph::State compute(dependency_graph::Values& data) {
@@ -53,8 +53,8 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 
 	// TODO: for parallelization to work reliably, we need to use integer atomics here, unfortunately
 
-	cv::Mat mat = cv::Mat::zeros(height, width, CV_32FC3);
-	cv::Mat norm = cv::Mat::zeros(height, width, CV_32FC3);
+	cv::Mat mat = cv::Mat::zeros(height*elements, width*elements, CV_32FC3);
+	cv::Mat norm = cv::Mat::zeros(height*elements, width*elements, CV_16UC3);
 
 	tbb::parallel_for(0, input.rows, [&](int y) {
 		for(int x = 0; x < input.cols; ++x) {
@@ -62,11 +62,8 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 
 			const double uv_magnitude_2 = sample[2]*sample[2] + sample[3]*sample[3];
 			if(uv_magnitude_2 < 1.0) {
-				float target_x = sample[0] / (float)pattern.sensorResolution()[0] * (float)width / (float)elements;
-				float target_y = sample[1] / (float)pattern.sensorResolution()[1] * (float)height / (float)elements;
-
-				target_x += floor((sample[2] + 1.0) / 2.0 * (float)elements) * (float)width / (float)elements;
-				target_y += floor((sample[3] + 1.0) / 2.0 * (float)elements) * (float)height / (float)elements;
+				float target_x = sample[0] / (float)pattern.sensorResolution()[0] * (float)width;
+				float target_y = sample[1] / (float)pattern.sensorResolution()[1] * (float)height;
 
 				target_x = std::min(target_x, (float)(width-1));
 				target_y = std::min(target_y, (float)(height-1));
@@ -74,10 +71,13 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 				target_x = std::max(target_x, 0.0f);
 				target_y = std::max(target_y, 0.0f);
 
+				target_x += floor((sample[2] + 1.0) / 2.0 * (float)elements) * (float)width;
+				target_y += floor((sample[3] + 1.0) / 2.0 * (float)elements) * (float)height;
+
 				const int colorId = (x%2) + (y%2); // hardcoded bayer pattern, for now
 
 				float* color = mat.ptr<float>(floor(target_y), floor(target_x));
-				float* n = norm.ptr<float>(floor(target_y), floor(target_x));
+				uint16_t* n = norm.ptr<uint16_t>(floor(target_y), floor(target_x));
 
 				const float* value = input.ptr<float>(y, x);
 				(*applyFn)(color, n, colorId, value);
@@ -88,11 +88,12 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 	tbb::parallel_for(0, mat.rows, [&](int y) {
 		for(int x=0;x<mat.cols;++x)
 			for(int a=0;a<3;++a)
-				if(norm.ptr<float>(y,x)[a] > 0.0f)
-					mat.ptr<float>(y,x)[a] /= norm.ptr<float>(y,x)[a];
+				if(norm.ptr<uint16_t>(y,x)[a] > 0.0f)
+					mat.ptr<float>(y,x)[a] /= (float)norm.ptr<uint16_t>(y,x)[a];
 	});
 
 	data.set(a_out, possumwood::opencv::Frame(mat));
+	data.set(a_norm, possumwood::opencv::Frame(norm));
 
 	return dependency_graph::State();
 }
@@ -103,11 +104,17 @@ void init(possumwood::Metadata& meta) {
 	meta.addAttribute(a_size, "size", Imath::Vec2<unsigned>(300u, 300u));
 	meta.addAttribute(a_elements, "elements", 5u);
 	meta.addAttribute(a_out, "out_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
+	meta.addAttribute(a_norm, "sample_count", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
 
 	meta.addInfluence(a_in, a_out);
 	meta.addInfluence(a_pattern, a_out);
 	meta.addInfluence(a_size, a_out);
 	meta.addInfluence(a_elements, a_out);
+
+	meta.addInfluence(a_in, a_norm);
+	meta.addInfluence(a_pattern, a_norm);
+	meta.addInfluence(a_size, a_out);
+	meta.addInfluence(a_elements, a_norm);
 
 	meta.setCompute(compute);
 }
--- src/plugins/opencv/nodes/lightfields/integrate_nearest.cpp
+++ src/plugins/opencv/nodes/lightfields/integrate_nearest.cpp
@@ -16,22 +16,22 @@ namespace {
 dependency_graph::InAttr<possumwood::opencv::Frame> a_in;
 dependency_graph::InAttr<possumwood::opencv::LightfieldSamples> a_samples;
 dependency_graph::InAttr<Imath::Vec2<unsigned>> a_size;
-dependency_graph::OutAttr<possumwood::opencv::Frame> a_out;
+dependency_graph::OutAttr<possumwood::opencv::Frame> a_out, a_norm;
 
-void add1channel(float* color, float* n, int colorIndex, const float* value) {
+void add1channel(float* color, uint16_t* n, int colorIndex, const float* value) {
 	color[colorIndex] += value[0];
-	n[colorIndex] += 1.0f;
+	++n[colorIndex];
 }
 
-void add3channel(float* color, float* n, int colorIndex, const float* value) {
+void add3channel(float* color, uint16_t* n, int colorIndex, const float* value) {
 	color[0] += value[0];
-	n[0] += 1.0f;
+	++n[0];
 
 	color[1] += value[1];
-	n[1] += 1.0f;
+	++n[1];
 
 	color[2] += value[2];
-	n[2] += 1.0f;
+	++n[2];
 }
 
 dependency_graph::State compute(dependency_graph::Values& data) {
@@ -52,7 +52,7 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 	// TODO: for parallelization to work reliably, we need to use integer atomics here, unfortunately
 
 	cv::Mat mat = cv::Mat::zeros(height, width, CV_32FC3);
-	cv::Mat norm = cv::Mat::zeros(height, width, CV_32FC3);
+	cv::Mat norm = cv::Mat::zeros(height, width, CV_16UC3);
 
 	tbb::parallel_for(0, input.rows, [&](int y) {
 		const auto end = samples.end(y);
@@ -72,7 +72,7 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 			assert(floor(target_y) < height);
 
 			float* color = mat.ptr<float>(floor(target_y), floor(target_x));
-			float* n = norm.ptr<float>(floor(target_y), floor(target_x));
+			uint16_t* n = norm.ptr<uint16_t>(floor(target_y), floor(target_x));
 
 			const float* value = input.ptr<float>(it->source[1], it->source[0]);
 			(*applyFn)(color, n, it->color, value);
@@ -83,10 +83,11 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 		for(int x=0;x<mat.cols;++x)
 			for(int a=0;a<3;++a)
 				if(norm.ptr<float>(y,x)[a] > 0.0f)
-					mat.ptr<float>(y,x)[a] /= norm.ptr<float>(y,x)[a];
+					mat.ptr<float>(y,x)[a] /= (float)norm.ptr<uint16_t>(y,x)[a];
 	});
 
 	data.set(a_out, possumwood::opencv::Frame(mat));
+	data.set(a_norm, possumwood::opencv::Frame(norm));
 
 	return dependency_graph::State();
 }
@@ -96,9 +97,14 @@ void init(possumwood::Metadata& meta) {
 	meta.addAttribute(a_samples, "samples", possumwood::opencv::LightfieldSamples(), possumwood::AttrFlags::kVertical);
 	meta.addAttribute(a_size, "size", Imath::Vec2<unsigned>(300u, 300u));
 	meta.addAttribute(a_out, "out_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
+	meta.addAttribute(a_norm, "sample_count", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
 
 	meta.addInfluence(a_in, a_out);
 	meta.addInfluence(a_samples, a_out);
+	meta.addInfluence(a_size, a_out);
+
+	meta.addInfluence(a_in, a_norm);
+	meta.addInfluence(a_samples, a_norm);
 	meta.addInfluence(a_size, a_out);
 
 	meta.setCompute(compute);
