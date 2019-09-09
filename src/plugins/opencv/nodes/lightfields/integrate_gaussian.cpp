68274ba223cde7483ac0c416dc23d69d9b19c0f8
Martin Prazak <martin.prazak@gmail.com>
2019-09-09 22:19:25 +0100

    integrate_gaussian now uses vec2<unsigned> for size

--- src/plugins/opencv/nodes/lightfields/integrate_gaussian.cpp
+++ src/plugins/opencv/nodes/lightfields/integrate_gaussian.cpp
@@ -6,6 +6,7 @@
 
 #include <actions/traits.h>
 
+#include "maths/io/vec2.h"
 #include "frame.h"
 #include "lightfield_samples.h"
 #include "tools.h"
@@ -14,7 +15,7 @@ namespace {
 
 dependency_graph::InAttr<possumwood::opencv::Frame> a_in;
 dependency_graph::InAttr<possumwood::opencv::LightfieldSamples> a_samples;
-dependency_graph::InAttr<unsigned> a_width, a_height;
+dependency_graph::InAttr<Imath::Vec2<unsigned>> a_size;
 dependency_graph::InAttr<float> a_sigma2;
 dependency_graph::OutAttr<possumwood::opencv::Frame> a_out;
 
@@ -46,8 +47,8 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 
 	const cv::Mat& input = *data.get(a_in);
 
-	const unsigned width = data.get(a_width);
-	const unsigned height = data.get(a_height);
+	const unsigned width = data.get(a_size)[0];
+	const unsigned height = data.get(a_size)[1];
 
 	// TODO: for parallelization to work reliably, we need to use integer atomics here, unfortunately
 
@@ -97,15 +98,13 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 void init(possumwood::Metadata& meta) {
 	meta.addAttribute(a_in, "in_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
 	meta.addAttribute(a_samples, "samples", possumwood::opencv::LightfieldSamples(), possumwood::AttrFlags::kVertical);
-	meta.addAttribute(a_width, "size/width", 300u);
-	meta.addAttribute(a_height, "size/height", 300u);
+	meta.addAttribute(a_size, "size", Imath::Vec2<unsigned>(300u, 300u));
 	meta.addAttribute(a_sigma2, "sigma2", 2.0f);
 	meta.addAttribute(a_out, "out_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
 
 	meta.addInfluence(a_in, a_out);
 	meta.addInfluence(a_samples, a_out);
-	meta.addInfluence(a_width, a_out);
-	meta.addInfluence(a_height, a_out);
+	meta.addInfluence(a_size, a_out);
 	meta.addInfluence(a_sigma2, a_out);
 
 	meta.setCompute(compute);
