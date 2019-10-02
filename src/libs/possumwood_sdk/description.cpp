249bd3479ed9296261213dde87392aa73b499e47
Martin Prazak <martin.prazak@gmail.com>
2019-10-02 23:48:26 +0100

    Description now handles links and inline code

--- src/libs/possumwood_sdk/description.cpp
+++ src/libs/possumwood_sdk/description.cpp
@@ -51,15 +51,81 @@ std::string Description::html() const {
 		std::getline(ss, line);
 
 		if(starts_with(line, "# "))
-			result += "<h1>" + line.substr(2) + "</h1>\n";
+			line = "<h1>" + line.substr(2) + "</h1>\n";
 		else if(starts_with(line, "## "))
-			result += "<h2>" + line.substr(3) + "</h2>\n";
+			line = "<h2>" + line.substr(3) + "</h2>\n";
 		else if(starts_with(line, "### "))
-			result += "<h3>" + line.substr(4) + "</h3>\n";
+			line = "<h3>" + line.substr(4) + "</h3>\n";
 		else if(starts_with(line, "#### "))
-			result += "<h4>" + line.substr(5) + "</h4>\n";
+			line = "<h4>" + line.substr(5) + "</h4>\n";
 		else if(!line.empty())
-			result += "<p>" + line + "</p>\n";
+			line = "<p>" + line + "</p>\n";
+
+		{
+			std::string link_text, link;
+			int state = 0;
+			std::size_t start = 0;
+
+			for(std::size_t i=0; i<line.length(); ++i) {
+				switch(state) {
+					case 0:
+						if(line[i] == '[') {
+							state = 1;
+							start = i;
+						}
+						break;
+
+					case 1:
+						if(line[i] == ']')
+							state = 2;
+						else
+							link_text += line[i];
+						break;
+
+					case 2:
+						if(line[i] == '(')
+							state = 3;
+						else
+							state = 0;
+						break;
+
+					case 3:
+						if(line[i] == ')')
+							state = 4;
+						else
+							link += line[i];
+						break;
+
+					case 4:
+						line = line.substr(0, start) + "<a href=\"" + link + "\">" + link_text + "</a>" + line.substr(i);
+						i = 0;
+						state = 0;
+						break;
+				};
+			}
+		}
+
+		{
+			int state = 0;
+			for(std::size_t i=0; i<line.length(); ++i) {
+				switch(state) {
+					case 0:
+						if(line[i] == '`') {
+							line = line.substr(0, i) + "<code>" + line.substr(i+1);
+							state = 1;
+						}
+						break;
+					case 1:
+						if(line[i] == '`') {
+							line = line.substr(0, i) + "</code>" + line.substr(i+1);
+							state = 0;
+						}
+						break;
+				}
+			}
+		}
+
+		result += line;
 	}
 
 	return result;
