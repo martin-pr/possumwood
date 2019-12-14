ef064d0e3d81e91134223e01cdf9b648b129d4fb
Martin Prazak <martin.prazak@gmail.com>
2019-12-14 19:32:37 +0000

    Added opencv/magnitude and opencv/log nodes

---
+++ src/plugins/opencv/nodes/dft/log.cpp
@@ -0,0 +1,39 @@
+#include <possumwood_sdk/node_implementation.h>
+#include <opencv2/opencv.hpp>
+
+#include <actions/traits.h>
+
+#include "frame.h"
+
+namespace {
+
+dependency_graph::InAttr<possumwood::opencv::Frame> a_inFrame;
+dependency_graph::InAttr<float> a_offset;
+dependency_graph::OutAttr<possumwood::opencv::Frame> a_outFrame;
+
+dependency_graph::State compute(dependency_graph::Values& data) {
+	cv::Mat result = (*data.get(a_inFrame)).clone();
+
+	result += cv::Scalar::all(data.get(a_offset));
+
+	cv::log(result, result);
+
+	data.set(a_outFrame, possumwood::opencv::Frame(result));
+
+	return dependency_graph::State();
+}
+
+void init(possumwood::Metadata& meta) {
+	meta.addAttribute(a_inFrame, "in_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
+	meta.addAttribute(a_offset, "offset", 1.0f);
+	meta.addAttribute(a_outFrame, "out_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
+
+	meta.addInfluence(a_inFrame, a_outFrame);
+	meta.addInfluence(a_offset, a_outFrame);
+
+	meta.setCompute(compute);
+}
+
+possumwood::NodeImplementation s_impl("opencv/dft/log", init);
+
+}
