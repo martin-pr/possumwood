62779ba536b71a0fff8eec8eef09b487b1707fb1
Martin Prazak <martin.prazak@gmail.com>
2020-01-09 23:23:17 +0000

    New matrix-based pattern

--- src/libs/lightfields/pattern_new.cpp
+++ src/libs/lightfields/pattern_new.cpp
@@ -1,13 +1,12 @@
-#include "pattern_lytro.h"
+#include "pattern_new.h"
 
 #include <cmath>
 
 namespace lightfields {
 
-LytroPattern::LytroPattern(double lensPitch, double pixelPitch, double rotation,
-	Imath::V2d scaleFactor, Imath::V3d sensorOffset, Imath::V2i sensorResolution) : Pattern(sensorResolution), m_lensPitch(lensPitch/pixelPitch)
+NewPattern::NewPattern(double lensPitch, double pixelPitch, double rotation,
+	Imath::V2d scaleFactor, Imath::V3d sensorOffset, Imath::V2i sensorResolution) : Pattern(sensorResolution)
 {
-	// put together lens transformation matrix
 	const double cs = cos(rotation);
 	const double sn = sin(rotation);
 
@@ -30,30 +29,25 @@ LytroPattern::LytroPattern(double lensPitch, double pixelPitch, double rotation,
 	pattern[1][0] = -0.5 / sqrt(3.0/4.0);
 	pattern[1][1] = 1.0 / sqrt(3.0/4.0);
 
-	// we need both forward and backward projection
 	m_tr = rotate * transform * scale * pattern;
 	m_trInv = m_tr.inverse();
 }
 
-Imath::V4d LytroPattern::sample(const Imath::V2i& pixelPos) const {
+Imath::V4d NewPattern::sample(const Imath::V2i& pixelPos) const {
 	Imath::V4d result;
 
-	// convert the pixel position to lens space
 	const Imath::V3d pos = Imath::V3d(pixelPos[0], pixelPos[1], 1.0) * m_tr;
 
-	// surrounding lens position by rounding the pos in lens space
 	const Imath::V3d pos1 = Imath::V3d(floor(pos[0]), floor(pos[1]), 1.0);
 	const Imath::V3d pos2 = Imath::V3d(floor(pos[0]), ceil(pos[1]), 1.0);
 	const Imath::V3d pos3 = Imath::V3d(ceil(pos[0]), floor(pos[1]), 1.0);
 	const Imath::V3d pos4 = Imath::V3d(ceil(pos[0]), ceil(pos[1]), 1.0);
 
-	// convert back from lens space to to pixel space
 	const Imath::V3d lens1 = pos1 * m_trInv;
 	const Imath::V3d lens2 = pos2 * m_trInv;
 	const Imath::V3d lens3 = pos3 * m_trInv;
 	const Imath::V3d lens4 = pos4 * m_trInv;
 
-	// find the nearest lens center in pixel space
 	const float dist1 = std::pow(pixelPos[0] - lens1[0], 2) + std::pow(pixelPos[1] - lens1[1], 2);
 	const float dist2 = std::pow(pixelPos[0] - lens2[0], 2) + std::pow(pixelPos[1] - lens2[1], 2);
 	const float dist3 = std::pow(pixelPos[0] - lens3[0], 2) + std::pow(pixelPos[1] - lens3[1], 2);
@@ -74,13 +68,13 @@ Imath::V4d LytroPattern::sample(const Imath::V2i& pixelPos) const {
 		pos0 = pos4;
 	}
 
-	// lens center
+	// std::cout << dist1 << "  " << dist2 << "  " << dist3 << "  " << dist4 << std::endl;
+
 	result[0] = lens[0];
 	result[1] = lens[1];
 
-	// and pixel offset from lens center
-	result[2] = (pixelPos[0] - lens[0]) / m_lensPitch * 2.0;
-	result[3] = (pixelPos[1] - lens[1]) / m_lensPitch * 2.0;
+	result[2] = pixelPos[0] - lens[0];
+	result[3] = pixelPos[1] - lens[1];
 
 	return result;
 }
