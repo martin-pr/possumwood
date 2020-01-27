dbfd5cf4d40b25c0659cd55cfc276cdab1f272b7
Martin Prazak <martin.prazak@gmail.com>
2020-01-27 23:41:05 +0000

    dependency_graph::Data - moved private classes back to the private section, and removed debugging code

--- src/libs/dependency_graph/data.h
+++ src/libs/dependency_graph/data.h
@@ -13,44 +13,6 @@
 
 namespace dependency_graph {
 
-template<typename T>
-struct Factory {
-	Factory();
-};
-
-struct Holder {
-	virtual ~Holder() {};
-
-	virtual const std::type_info& typeinfo() const = 0;
-	virtual bool isEqual(const Holder& src) const = 0;
-	virtual std::string toString() const = 0;
-};
-
-template<typename T>
-class TypedHolder : public Holder {
-	public:
-		TypedHolder(const T& d);
-		virtual ~TypedHolder();
-
-		virtual const std::type_info& typeinfo() const override;
-		virtual bool isEqual(const Holder& src) const override;
-		virtual std::string toString() const override;
-
-		T data;
-		static Factory<T> m_factory;
-};
-
-template<typename T>
-struct Maker {
-	static std::shared_ptr<const Holder> make(const T& val) {
-		return std::shared_ptr<const Holder>(new TypedHolder<T>(val));
-	}
-};
-
-template<>
-struct Maker<decltype(nullptr)> {
-};
-
 class Data {
 	public:
 		/// creates a new Data instance based on type name. Internally implements a factory mechanism to produce instances based on named types.
@@ -61,8 +23,7 @@ class Data {
 
 		// creates a data instance of a particular type initialised to a value
 		template<typename T>
-		Data(const T& value) : m_data(Maker<T>::make(value)) {
-		}
+		Data(const T& value);
 
 		Data(const Data& d);
 		Data& operator = (const Data& d);
@@ -82,14 +43,39 @@ class Data {
 		bool operator == (const Data& d) const;
 		bool operator != (const Data& d) const;
 
-		static std::map<std::string, std::function<Data()>>& factories();
-
-	protected:
+	private:
 		std::string toString() const;
 
-		std::shared_ptr<const Holder> m_data;
+		template<typename T>
+		struct Factory {
+			Factory();
+		};
 
-	private:
+		static std::map<std::string, std::function<Data()>>& factories();
+
+		struct Holder {
+			virtual ~Holder() {};
+
+			virtual const std::type_info& typeinfo() const = 0;
+			virtual bool isEqual(const Holder& src) const = 0;
+			virtual std::string toString() const = 0;
+		};
+
+		template<typename T>
+		class TypedHolder : public Holder {
+			public:
+				TypedHolder(const T& d);
+				virtual ~TypedHolder();
+
+				virtual const std::type_info& typeinfo() const override;
+				virtual bool isEqual(const Holder& src) const override;
+				virtual std::string toString() const override;
+
+				T data;
+				static Factory<T> m_factory;
+		};
+
+		std::shared_ptr<const Holder> m_data;
 
 	friend std::ostream& operator << (std::ostream& out, const Data& bd);
 };
--- src/libs/dependency_graph/data.inl
+++ src/libs/dependency_graph/data.inl
@@ -9,11 +9,11 @@
 namespace dependency_graph {
 
 template<typename T>
-Factory<T> TypedHolder<T>::m_factory;
+Data::Factory<T> Data::TypedHolder<T>::m_factory;
 
-// template<typename T>
-// Data::Data(const T& value) : m_data(new TypedHolder<T>(value)) {
-// }
+template<typename T>
+Data::Data(const T& value) : m_data(new TypedHolder<T>(value)) {
+}
 
 template<typename T>
 const T& Data::get() const {
@@ -49,7 +49,7 @@ Data makeData<void>() {
 }
 
 template<typename T>
-Factory<T>::Factory() {
+Data::Factory<T>::Factory() {
 	Data::factories().insert(std::make_pair(unmangledTypeId<T>(), []() {
 		return makeData<T>();
 	}));
@@ -58,29 +58,29 @@ Factory<T>::Factory() {
 ////////////
 
 template<typename T>
-TypedHolder<T>::TypedHolder(const T& d) : data(d) {
+Data::TypedHolder<T>::TypedHolder(const T& d) : data(d) {
 }
 
 template<typename T>
-TypedHolder<T>::~TypedHolder() {
+Data::TypedHolder<T>::~TypedHolder() {
 	// just do something with the factory, to make sure it is instantiated
 	std::stringstream ss;
 	ss << &m_factory;
 }
 
 template<typename T>
-const std::type_info& TypedHolder<T>::typeinfo() const {
+const std::type_info& Data::TypedHolder<T>::typeinfo() const {
 	return typeid(T);
 }
 
 template<typename T>
-bool TypedHolder<T>::isEqual(const Holder& src) const {
+bool Data::TypedHolder<T>::isEqual(const Holder& src) const {
 	const TypedHolder<T>* h2 = dynamic_cast<const TypedHolder<T>*>(&src);
 	return h2 != nullptr && DataTraits<T>::isEqual(data, h2->data);
 }
 
 template<typename T>
-std::string TypedHolder<T>::toString() const {
+std::string Data::TypedHolder<T>::toString() const {
 	std::stringstream ss;
 	ss << data;
 	return ss.str();
