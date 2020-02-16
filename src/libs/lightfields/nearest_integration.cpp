15481cf23309309f4bd37bf82e08b1c8b13ddfcf
Martin Prazak <martin.prazak@gmail.com>
2020-02-16 18:47:04 +0000

    Nearest integration moved to the lightfields library

---
+++ src/libs/lightfields/nearest_integration.cpp
@@ -0,0 +1,83 @@
+#include "nearest_integration.h"
+
+#include <tbb/parallel_for.h>
+
+#include "samples.h"
+
+namespace lightfields { namespace nearest {
+
+namespace {
+
+void add1channel(float* color, uint16_t* n, int colorIndex, const float* value) {
+	color[colorIndex] += value[0];
+	++n[colorIndex];
+}
+
+void add3channel(float* color, uint16_t* n, int colorIndex, const float* value) {
+	color[0] += value[0];
+	++n[0];
+
+	color[1] += value[1];
+	++n[1];
+
+	color[2] += value[2];
+	++n[2];
+}
+
+}
+
+Result integrate(const lightfields::Samples& samples, const Imath::Vec2<unsigned>& size, const cv::Mat& input) {
+	if(input.type() != CV_32FC3 && input.type() != CV_32FC1)
+		throw std::runtime_error("Nearest neighbour integration - only 1 or 3 channel float data allowed on input.");
+
+	auto applyFn = &add1channel;
+	if(input.type() == CV_32FC3)
+		applyFn = &add3channel;
+
+	const unsigned width = size[0];
+	const unsigned height = size[1];
+
+	// TODO: for parallelization to work reliably, we need to use integer atomics here, unfortunately
+
+	cv::Mat average = cv::Mat::zeros(height, width, CV_32FC3);
+	cv::Mat norm = cv::Mat::zeros(height, width, CV_16UC3);
+
+	const float x_scale = (float)width / (float)samples.sensorSize()[0];
+	const float y_scale = (float)height / (float)samples.sensorSize()[1];
+
+	tbb::parallel_for(0, input.rows, [&](int y) {
+		const auto begin = samples.begin(y);
+		const auto end = samples.end(y);
+		assert(begin <= end);
+
+		for(auto it = begin; it != end; ++it) {
+			assert(it->source[1] == y);
+
+			const float target_x = it->xy[0] * x_scale;
+			const float target_y = it->xy[1] * y_scale;
+
+			// sanity checks
+			assert(it->source[0] < input.rows);
+			assert(it->source[1] < input.cols);
+
+			if((floor(target_x) >= 0) && (floor(target_y) >= 0) && (floor(target_x) < width) && (floor(target_y) < height)) {
+				float* color = average.ptr<float>(floor(target_y), floor(target_x));
+				uint16_t* n = norm.ptr<uint16_t>(floor(target_y), floor(target_x));
+
+				const float* value = input.ptr<float>(it->source[1], it->source[0]);
+				(*applyFn)(color, n, it->color, value);
+			}
+		}
+	});
+
+	tbb::parallel_for(0, average.rows, [&](int y) {
+		for(int x=0;x<average.cols;++x)
+			for(int a=0;a<3;++a)
+				if(norm.ptr<uint16_t>(y,x)[a] > 0)
+					average.ptr<float>(y,x)[a] /= (float)norm.ptr<uint16_t>(y,x)[a];
+	});
+
+	return Result { average, norm };
+}
+
+} }
---
+++ src/libs/lightfields/nearest_integration.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <ImathVec.h>
+
+namespace lightfields {
+
+class Samples;
+
+namespace nearest {
+
+struct Result {
+	cv::Mat average; ///< resulting RGB image (float, 3 channels)
+	cv::Mat samples; ///< per-pixel sample count (unsigned int 16, 3 channels)
+};
+
+/// Integrates an image - produces an image using nearest-neighbour integration
+Result integrate(const lightfields::Samples& samples, const Imath::Vec2<unsigned>& size, const cv::Mat& data);
+
+} }
