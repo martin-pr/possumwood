ec4c7a8675f8147a03cee350a1b4d5b81e10d8a4
Martin Prazak <martin.prazak@gmail.com>
2020-03-13 19:09:20 +0000

    Added Max, Min-Max and Sum normalization modes to sequence/pixel_normalize

--- src/plugins/opencv/nodes/sequence/normalize.cpp
+++ src/plugins/opencv/nodes/sequence/normalize.cpp
@@ -2,12 +2,21 @@
 
 #include <tbb/parallel_for.h>
 
+#include "possumwood_sdk/datatypes/enum.h"
+
 #include "sequence.h"
 
 namespace {
 
 dependency_graph::InAttr<possumwood::opencv::Sequence> a_in;
+dependency_graph::InAttr<possumwood::Enum> a_mode;
 dependency_graph::OutAttr<possumwood::opencv::Sequence> a_out;
+
+enum Mode {
+	kMinMax,
+	kMax,
+	kSum
+};
 
 dependency_graph::State compute(dependency_graph::Values& data) {
 	const possumwood::opencv::Sequence& in = data.get(a_in);
@@ -15,7 +24,7 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 	if(!in.isValid())
 		throw std::runtime_error("Input sequence does not have consistent size or type.");
 
-	possumwood::opencv::Sequence out = in;
+	possumwood::opencv::Sequence out = in.clone();
 
 	if(!in.empty()) {
 		if(in.type() != CV_32FC1)
@@ -24,24 +33,46 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 		const int cols = in.cols();
 		const int rows = in.rows();
 
+		Mode mode = kMinMax;
+		if(data.get(a_mode).value() == "Maximum")
+			mode = kMax;
+		else if(data.get(a_mode).value() == "Sum")
+			mode = kSum;
+
 		tbb::parallel_for(0, rows, [&](int r) {
 			for(int c=0; c<cols; ++c) {
 				auto it = out.begin();
+
 				float max = (*it)->at<float>(r, c);
-				float min = (*it)->at<float>(r, c);
+				float min = max;
+				float sum = max;
 				++it;
 
 				while(it != out.end()) {
-					max = std::max(max, (*it)->at<float>(r, c));
-					min = std::min(min, (*it)->at<float>(r, c));
+					const float& tmp = (*it)->at<float>(r, c);
+
+					max = std::max(max, tmp);
+					min = std::min(min, tmp);
+					sum += tmp;
 
 					++it;
 				}
 
-				for(it = out.begin(); it != out.end(); ++it) {
-					float& val = (*it)->at<float>(r, c);
-					val = (val - min) / (max - min);
-				}
+				if(mode == kMinMax)
+					for(it = out.begin(); it != out.end(); ++it) {
+						float& val = (*it)->at<float>(r, c);
+						val = (val - min) / (max - min);
+					}
+				else if(mode == kMax)
+					for(it = out.begin(); it != out.end(); ++it) {
+						float& val = (*it)->at<float>(r, c);
+						val /= max;
+					}
+				else if(mode == kSum)
+					for(it = out.begin(); it != out.end(); ++it) {
+						float& val = (*it)->at<float>(r, c);
+						val /= sum;
+					}
 			}
 		});
 	}
@@ -53,9 +84,12 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 
 void init(possumwood::Metadata& meta) {
 	meta.addAttribute(a_in, "in");
+	meta.addAttribute(a_mode, "mode",
+	                  possumwood::Enum({"Minimum-maximum", "Maximum", "Sum"}));
 	meta.addAttribute(a_out, "out");
 
 	meta.addInfluence(a_in, a_out);
+	meta.addInfluence(a_mode, a_out);
 
 	meta.setCompute(compute);
 }
