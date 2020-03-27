879dea230233b853d489a257aed58bf2bb83acaa
Martin Prazak <martin.prazak@gmail.com>
2020-03-27 21:01:24 +0000

    Reimplemented Laplacian filter convolution - need the core in a separate file to allow for sequence processing

---
+++ src/plugins/opencv/laplacian.cpp
@@ -0,0 +1,131 @@
+#include "laplacian.h"
+
+namespace possumwood { namespace opencv {
+
+namespace {
+
+cv::Mat normalize(const cv::Mat& in) {
+	cv::Mat result = in.clone();
+
+	float sum = 0.0f;
+	for(int y=0;y<in.rows;++y)
+		for(int x=0;x<in.cols;++x)
+			sum += std::abs(result.at<float>(y, x));
+
+	for(int y=0;y<in.rows;++y)
+		for(int x=0;x<in.cols;++x)
+			result.at<float>(y, x) /= sum;
+
+	return result;
+}
+
+static const cv::Mat kernel_4cross = normalize((cv::Mat_<float>(3,3) <<
+	 0.0, -1.0,  0.0,
+	-1.0,  4.0, -1.0,
+	 0.0, -1.0,  0.0
+));
+
+static const cv::Mat kernel_8uniform = normalize((cv::Mat_<float>(3,3) <<
+	-1.0, -1.0, -1.0,
+	-1.0,  8.0, -1.0,
+	-1.0, -1.0, -1.0
+));
+
+static const cv::Mat kernel_8shaped = normalize((cv::Mat_<float>(3,3) <<
+	-1.0, -2.0, -1.0,
+	-2.0, 12.0, -2.0,
+	-1.0, -2.0, -1.0
+));
+
+static const cv::Mat kernel_24 = normalize((cv::Mat_<float>(5,5) <<
+	 0.0,  0.0,  1.0,  0.0,  0.0,
+	 0.0,  2.0, -8.0,  2.0,  0.0,
+	 1.0, -8.0, 20.0, -8.0,  1.0,
+	 0.0,  2.0, -8.0,  2.0,  0.0,
+	 0.0,  0.0,  1.0,  0.0,  0.0
+));
+
+const cv::Mat& kernel(LaplacianKernel k) {
+	switch(k) {
+		case k4Cross: return kernel_4cross;
+		case k8Uniform: return kernel_8uniform;
+		case k8Shaped: return kernel_8shaped;
+		case k24Shaped: return kernel_24;
+
+		default:
+			assert(false);
+			static const cv::Mat s_dummy;
+			return s_dummy;
+	};
+}
+
+void convolution(const cv::Mat& src, cv::Mat& tgt, const cv::Mat& kernel) {
+	assert(src.rows == tgt.rows && src.cols == tgt.cols);
+	assert(src.channels() == 1);
+	assert(tgt.channels() == 1);
+	assert(src.type() == CV_32FC1 && tgt.type() == CV_32FC1 && kernel.type() == CV_32FC1);
+
+	for(int y=0; y<src.rows; ++y)
+		for(int x=0; x<src.cols; ++x) {
+			float& target = tgt.at<float>(y, x);
+
+			// convolution
+			for(int yi=0; yi<kernel.rows; ++yi)
+				for(int xi=0; xi<kernel.cols; ++xi) {
+					int ypos = y + yi - kernel.rows/2;
+					int xpos = x + xi - kernel.cols/2;
+
+					// handling of edges - "clip" (or "mirror", commented out for now)
+					if(ypos < 0)
+						// ypos = -ypos;
+						ypos = 0;
+					if(ypos >= src.rows)
+						// ypos = (image.rows-1) - (ypos-image.rows);
+						ypos = src.rows - 1;
+
+					if(xpos < 0)
+						// xpos = -xpos;
+						xpos = 0;
+					if(xpos >= src.cols)
+						// xpos = (image.cols-1) - (xpos-image.cols);
+						xpos = src.cols - 1;
+
+					const float& k = kernel.at<float>(yi, xi);
+					const float& source = src.at<float>(ypos, xpos);
+
+					target += k * source;
+				}
+		}
+}
+
+}
+
+const std::vector<std::pair<std::string, int>>& laplacianKernels() {
+	static const std::vector<std::pair<std::string, int>> s_kernels({
+		std::pair<std::string, int>("4_cross", k4Cross),
+		std::pair<std::string, int>("8_uniform", k8Uniform),
+		std::pair<std::string, int>("8_shaped", k8Shaped),
+		std::pair<std::string, int>("24_shaped", k24Shaped),
+	});
+
+	return s_kernels;
+}
+
+cv::Mat laplacian(const cv::Mat& src, LaplacianKernel k) {
+	assert(src.depth() == CV_32F);
+
+	cv::Mat result = cv::Mat::zeros(src.rows, src.cols, src.type());
+
+	std::vector<cv::Mat> tmp_src(src.channels()), tmp_dest(src.channels());
+	cv::split(src, tmp_src);
+	cv::split(result, tmp_dest);
+
+	for(int c=0; c<src.channels(); ++c)
+		convolution(tmp_src[c], tmp_dest[c], kernel(k));
+
+	cv::merge(tmp_dest, result);
+
+	return result;
+}
+
+}}
--- src/plugins/opencv/nodes/laplacian_smoothness.cpp
+++ src/plugins/opencv/nodes/laplacian_smoothness.cpp
@@ -7,6 +7,7 @@
 
 #include "frame.h"
 #include "tools.h"
+#include "laplacian.h"
 
 namespace {
 
@@ -14,108 +15,13 @@ dependency_graph::InAttr<possumwood::opencv::Frame> a_in;
 dependency_graph::InAttr<possumwood::Enum> a_kernel;
 dependency_graph::OutAttr<possumwood::opencv::Frame> a_out;
 
-cv::Mat normalize(const cv::Mat& in) {
-	cv::Mat result = in.clone();
-
-	float sum = 0.0f;
-	for(int y=0;y<in.rows;++y)
-		for(int x=0;x<in.cols;++x)
-			sum += std::abs(result.at<float>(y, x));
-
-	for(int y=0;y<in.rows;++y)
-		for(int x=0;x<in.cols;++x)
-			result.at<float>(y, x) /= sum;
-
-	return result;
-}
-
-static const cv::Mat kernel_4cross = normalize((cv::Mat_<float>(3,3) <<
-	 0.0, -1.0,  0.0,
-	-1.0,  4.0, -1.0,
-	 0.0, -1.0,  0.0
-));
-
-static const cv::Mat kernel_8uniform = normalize((cv::Mat_<float>(3,3) <<
-	-1.0, -1.0, -1.0,
-	-1.0,  8.0, -1.0,
-	-1.0, -1.0, -1.0
-));
-
-static const cv::Mat kernel_8shaped = normalize((cv::Mat_<float>(3,3) <<
-	-1.0, -2.0, -1.0,
-	-2.0, 12.0, -2.0,
-	-1.0, -2.0, -1.0
-));
-
-static const cv::Mat kernel_24 = normalize((cv::Mat_<float>(5,5) <<
-	 0.0,  0.0,  1.0,  0.0,  0.0,
-	 0.0,  2.0, -8.0,  2.0,  0.0,
-	 1.0, -8.0, 20.0, -8.0,  1.0,
-	 0.0,  2.0, -8.0,  2.0,  0.0,
-	 0.0,  0.0,  1.0,  0.0,  0.0
-));
-
-const cv::Mat& kernelEnum(const std::string& mode) {
-	if(mode == "4_cross")
-		return kernel_4cross;
-	else if(mode == "8_uniform")
-		return kernel_8uniform;
-	else if(mode == "8_shaped")
-		return kernel_8shaped;
-	else if(mode == "24_shaped")
-		return kernel_24;
-
-	throw std::runtime_error("Unknown kernel " + mode);
-}
-
-void convolution(const cv::Mat& src, cv::Mat& tgt, const cv::Mat& kernel) {
-	assert(src.rows == tgt.rows && src.cols == tgt.cols);
-
-	for(int y=0; y<src.rows; ++y)
-		for(int x=0; x<src.cols; ++x) {
-			float& target = tgt.at<float>(y, x);
-
-			// convolution
-			for(int yi=0; yi<kernel.rows; ++yi)
-				for(int xi=0; xi<kernel.cols; ++xi) {
-					int ypos = y + yi - kernel.rows/2;
-					int xpos = x + xi - kernel.cols/2;
-
-					// handling of edges - "clip" (or "mirror", commented out for now)
-					if(ypos < 0)
-						// ypos = -ypos;
-						ypos = 0;
-					if(ypos >= src.rows)
-						// ypos = (image.rows-1) - (ypos-image.rows);
-						ypos = src.rows - 1;
-
-					if(xpos < 0)
-						// xpos = -xpos;
-						xpos = 0;
-					if(xpos >= src.cols)
-						// xpos = (image.cols-1) - (xpos-image.cols);
-						xpos = src.cols - 1;
-
-					const float& k = kernel.at<float>(yi, xi);
-					const float* source = src.ptr<float>(ypos, xpos);
-
-					for(int a=0;a<src.channels();++a)
-						target += k * source[a];
-				}
-
-			target = std::abs(target);
-		}
-}
-
 dependency_graph::State compute(dependency_graph::Values& data) {
 	const cv::Mat& in = *data.get(a_in);
 
 	if((in.type() != CV_32FC1) && (in.type() != CV_32FC3))
 		throw std::runtime_error("Only works with CV_32FC1 and CV_32FC3");
 
-	cv::Mat out = cv::Mat::zeros(in.rows, in.cols, CV_32FC1);
-
-	convolution(in, out, kernelEnum(data.get(a_kernel).value()));
+	const cv::Mat out = possumwood::opencv::laplacian(in, possumwood::opencv::LaplacianKernel(data.get(a_kernel).intValue()));
 
 	data.set(a_out, possumwood::opencv::Frame(out));
 
@@ -124,8 +30,7 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 
 void init(possumwood::Metadata& meta) {
 	meta.addAttribute(a_in, "in_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
-	meta.addAttribute(a_kernel, "kernel",
-		possumwood::Enum({"4_cross", "8_uniform", "8_shaped", "24_shaped"}));
+	meta.addAttribute(a_kernel, "kernel", possumwood::Enum(possumwood::opencv::laplacianKernels().begin(), possumwood::opencv::laplacianKernels().end()));
 	meta.addAttribute(a_out, "out_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
 
 	meta.addInfluence(a_in, a_out);
