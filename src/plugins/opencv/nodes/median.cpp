27547bd69a83dd04e6d474c9be893b2b2585a07c
Martin Prazak <martin.prazak@gmail.com>
2020-04-05 20:46:54 +0100

    Added median filter to OpenCV list of nodes

---
+++ src/plugins/opencv/nodes/median.cpp
@@ -0,0 +1,53 @@
+#include <possumwood_sdk/node_implementation.h>
+
+#include <opencv2/opencv.hpp>
+
+#include <actions/traits.h>
+
+#include "frame.h"
+
+namespace {
+
+dependency_graph::InAttr<possumwood::opencv::Frame> a_inFrame;
+dependency_graph::InAttr<unsigned> a_kernelSize;
+dependency_graph::OutAttr<possumwood::opencv::Frame> a_outFrame;
+
+static const std::vector<std::pair<std::string, int>> s_modes {
+	{"COLORMAP_AUTUMN", cv::COLORMAP_AUTUMN},
+	{"COLORMAP_BONE", cv::COLORMAP_BONE},
+	{"COLORMAP_JET", cv::COLORMAP_JET},
+	{"COLORMAP_WINTER", cv::COLORMAP_WINTER},
+	{"COLORMAP_RAINBOW", cv::COLORMAP_RAINBOW},
+	{"COLORMAP_OCEAN", cv::COLORMAP_OCEAN},
+	{"COLORMAP_SUMMER", cv::COLORMAP_SUMMER},
+	{"COLORMAP_SPRING", cv::COLORMAP_SPRING},
+	{"COLORMAP_COOL", cv::COLORMAP_COOL},
+	{"COLORMAP_HSV", cv::COLORMAP_HSV},
+	{"COLORMAP_PINK", cv::COLORMAP_PINK},
+	{"COLORMAP_HOT", cv::COLORMAP_HOT},
+};
+
+dependency_graph::State compute(dependency_graph::Values& data) {
+	cv::Mat result = (*data.get(a_inFrame)).clone();
+
+	cv::medianBlur(*data.get(a_inFrame), result, data.get(a_kernelSize));
+
+	data.set(a_outFrame, possumwood::opencv::Frame(result));
+
+	return dependency_graph::State();
+}
+
+void init(possumwood::Metadata& meta) {
+	meta.addAttribute(a_inFrame, "in_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
+	meta.addAttribute(a_kernelSize, "kernel_size", 5u);
+	meta.addAttribute(a_outFrame, "out_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
+
+	meta.addInfluence(a_inFrame, a_outFrame);
+	meta.addInfluence(a_kernelSize, a_outFrame);
+
+	meta.setCompute(compute);
+}
+
+possumwood::NodeImplementation s_impl("opencv/median", init);
+
+}
