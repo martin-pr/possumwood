eddeffca0ee5fde239e186d246ec36aeec1ee943
Martin Prazak <martin.prazak@gmail.com>
2020-04-17 09:42:50 +0100

    Extended lightfields::Grid to allow lightfields::Bitfield as its "backend"

--- src/libs/lightfields/grid.h
+++ src/libs/lightfields/grid.h
@@ -2,11 +2,12 @@
 
 #include <cassert>
 #include <vector>
+#include <type_traits>
 
 namespace lightfields {
 
 /// A trivial grid abstraction (not std-compliant, no need for now)
-template<typename VALUE>
+template<typename VALUE, typename CONTAINER = std::vector<VALUE>>
 class Grid {
 	public:
 		explicit Grid(unsigned rows, unsigned cols, const VALUE& value) : m_rows(rows), m_cols(cols), m_p(rows*cols, value) {
@@ -15,12 +16,12 @@ class Grid {
 		explicit Grid(unsigned rows, unsigned cols) : m_rows(rows), m_cols(cols), m_p(rows*cols) {
 		}
 
-		VALUE& operator() (unsigned row, unsigned col) {
+		decltype(std::declval<CONTAINER>()[0]) operator() (unsigned row, unsigned col) {
 			assert(row < m_rows && col < m_cols);
 			return m_p[row * m_cols + col];
 		}
 
-		const VALUE& operator() (unsigned row, unsigned col) const {
+		decltype(std::declval<const CONTAINER>()[0]) operator() (unsigned row, unsigned col) const {
 			assert(row < m_rows && col < m_cols);
 			return m_p[row * m_cols + col];
 		}
@@ -40,9 +41,17 @@ class Grid {
 			m_p.swap(g.m_p);
 		}
 
+		const CONTAINER& container() const {
+			return m_p;
+		}
+
+		CONTAINER& container() {
+			return m_p;
+		}
+
 	private:
 		unsigned m_rows, m_cols;
-		std::vector<VALUE> m_p;
+		CONTAINER m_p;
 };
 
 }
