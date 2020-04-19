e0bc8817e1aa0c532492750cca871de21ad16e80
Martin Prazak <martin.prazak@gmail.com>
2020-04-19 17:47:00 +0100

    opencv/demosaic - simplified modes enum with the newer API

--- src/plugins/opencv/nodes/demosaic.cpp
+++ src/plugins/opencv/nodes/demosaic.cpp
@@ -14,54 +14,34 @@ dependency_graph::InAttr<possumwood::opencv::Frame> a_inFrame;
 dependency_graph::InAttr<possumwood::Enum> a_mode;
 dependency_graph::OutAttr<possumwood::opencv::Frame> a_outFrame;
 
-int modeToEnum(const std::string& mode) {
-	if(mode == "BayerBG2BGR")
-		return cv::COLOR_BayerBG2BGR;
-	else if(mode == "BayerGB2BGR")
-		return cv::COLOR_BayerGB2BGR;
-	else if(mode == "BayerRG2BGR")
-		return cv::COLOR_BayerRG2BGR;
-	else if(mode == "BayerGR2BGR")
-		return cv::COLOR_BayerGR2BGR;
-	else if(mode == "BayerBG2BGR_VNG")
-		return cv::COLOR_BayerBG2BGR_VNG;
-	else if(mode == "BayerGB2BGR_VNG")
-		return cv::COLOR_BayerGB2BGR_VNG;
-	else if(mode == "BayerRG2BGR_VNG")
-		return cv::COLOR_BayerRG2BGR_VNG;
-	else if(mode == "BayerGR2BGR_VNG")
-		return cv::COLOR_BayerGR2BGR_VNG;
-	else if(mode == "BayerBG2BGR_EA")
-		return cv::COLOR_BayerBG2BGR_EA;
-	else if(mode == "BayerGB2BGR_EA")
-		return cv::COLOR_BayerGB2BGR_EA;
-	else if(mode == "BayerRG2BGR_EA")
-		return cv::COLOR_BayerRG2BGR_EA;
-	else if(mode == "BayerGR2BGR_EA")
-		return cv::COLOR_BayerGR2BGR_EA;
-
-	throw std::runtime_error("Unknown conversion mode " + mode);
-}
+static std::vector<std::pair<std::string, int>> s_modes {
+	{"BayerBG2BGR", cv::COLOR_BayerBG2BGR},
+	{"BayerGB2BGR", cv::COLOR_BayerGB2BGR},
+	{"BayerRG2BGR", cv::COLOR_BayerRG2BGR},
+	{"BayerGR2BGR", cv::COLOR_BayerGR2BGR},
+	{"BayerBG2BGR_VNG", cv::COLOR_BayerBG2BGR_VNG},
+	{"BayerGB2BGR_VNG", cv::COLOR_BayerGB2BGR_VNG},
+	{"BayerRG2BGR_VNG", cv::COLOR_BayerRG2BGR_VNG},
+	{"BayerGR2BGR_VNG", cv::COLOR_BayerGR2BGR_VNG},
+	{"BayerBG2BGR_EA", cv::COLOR_BayerBG2BGR_EA},
+	{"BayerGB2BGR_EA", cv::COLOR_BayerGB2BGR_EA},
+	{"BayerRG2BGR_EA", cv::COLOR_BayerRG2BGR_EA},
+	{"BayerGR2BGR_EA", cv::COLOR_BayerGR2BGR_EA},
+};
 
 dependency_graph::State compute(dependency_graph::Values& data) {
 	cv::Mat result;
 
-	cvtColor(*data.get(a_inFrame), result, modeToEnum(data.get(a_mode).value()));
+	cvtColor(*data.get(a_inFrame), result, data.get(a_mode).intValue());
 
 	data.set(a_outFrame, possumwood::opencv::Frame(result));
-	
+
 	return dependency_graph::State();
 }
 
 void init(possumwood::Metadata& meta) {
 	meta.addAttribute(a_inFrame, "in_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
-	meta.addAttribute(a_mode, "mode", 
-		possumwood::Enum({
-			"BayerBG2BGR", "BayerGB2BGR", "BayerRG2BGR", "BayerGR2BGR", 
-			"BayerBG2BGR_VNG", "BayerGB2BGR_VNG", "BayerRG2BGR_VNG", 
-			"BayerGR2BGR_VNG", "BayerBG2BGR_EA", "BayerGB2BGR_EA", 
-			"BayerRG2BGR_EA", "BayerGR2BGR_EA"
-		}));
+	meta.addAttribute(a_mode, "mode", possumwood::Enum(s_modes.begin(), s_modes.end()));
 	meta.addAttribute(a_outFrame, "out_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
 
 	meta.addInfluence(a_inFrame, a_outFrame);
