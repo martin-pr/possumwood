d9712950e2c0c346f2a1df5107760e42014b1328
Martin Prazak <martin.prazak@gmail.com>
2020-05-03 17:18:49 +0100

    Moved attributes to a separate struct

--- src/plugins/opencv/nodes/superpixels/mean.cpp
+++ src/plugins/opencv/nodes/superpixels/mean.cpp
@@ -3,14 +3,22 @@
 #include <tbb/parallel_for.h>
 
 #include <actions/traits.h>
+#include <possumwood_sdk/datatypes/enum.h>
 
 #include "frame.h"
 
 namespace {
 
-dependency_graph::InAttr<possumwood::opencv::Frame> a_in;
-dependency_graph::InAttr<possumwood::opencv::Frame> a_superpixels;
-dependency_graph::OutAttr<possumwood::opencv::Frame> a_out;
+struct MeanAttrs {
+	dependency_graph::InAttr<possumwood::opencv::Frame> a_in;
+	dependency_graph::InAttr<possumwood::opencv::Frame> a_superpixels;
+	dependency_graph::InAttr<possumwood::Enum> a_mode;
+	dependency_graph::OutAttr<possumwood::opencv::Frame> a_out;
+
+	void init(possumwood::Metadata& meta);
+};
+
+MeanAttrs s_meanAttrs;
 
 float getFloat(const cv::Mat& in, int row, int col, int channel) {
 	switch(in.depth()) {
@@ -38,6 +46,16 @@ void setFloat(cv::Mat& in, int row, int col, int channel, float val) {
 	}
 }
 
+enum Mode {
+	kMean,
+	kMedian,
+};
+
+static std::vector<std::pair<std::string, int>> s_mode {
+	{"mean", kMean},
+	{"median", kMedian},
+};
+
 class Mean {
 	public:
 		Mean() : m_val(0.0f), m_norm(0.0f) {
@@ -58,6 +76,27 @@ class Mean {
 
 	private:
 		float m_val, m_norm;
+};
+
+class Median {
+	public:
+		Median& operator += (float val) {
+			m_val.push_back(val);
+
+			return *this;
+		}
+
+		float operator*() {
+			if(m_val.empty())
+				return 0.0f;
+
+			std::sort(m_val.begin(), m_val.end());
+
+			return m_val[m_val.size()/2];
+		}
+
+	private:
+		std::vector<float> m_val;
 };
 
 template<typename MEAN>
@@ -93,9 +132,9 @@ cv::Mat process(const cv::Mat& in, const cv::Mat& superpixels) {
 	return out;
 }
 
-dependency_graph::State compute(dependency_graph::Values& data) {
-	const cv::Mat& in = *data.get(a_in);
-	const cv::Mat& superpixels = *data.get(a_superpixels);
+dependency_graph::State compute(dependency_graph::Values& data, MeanAttrs& attrs) {
+	const cv::Mat& in = *data.get(attrs.a_in);
+	const cv::Mat& superpixels = *data.get(attrs.a_superpixels);
 
 	if(in.rows != superpixels.rows || in.cols != superpixels.cols)
 		throw std::runtime_error("Input and superpixel size have to match.");
@@ -103,24 +142,34 @@ dependency_graph::State compute(dependency_graph::Values& data) {
 	if(superpixels.type() != CV_32SC1)
 		throw std::runtime_error("Only CV_32SC1 type supported on the superpixels input!");
 
-	const cv::Mat& out = process<Mean>(in, superpixels);
+	cv::Mat out;
+	if(data.get(attrs.a_mode).intValue() == kMean)
+		out = process<Mean>(in, superpixels);
+	else if(data.get(attrs.a_mode).intValue() == kMedian)
+		out = process<Median>(in, superpixels);
+	else
+		throw std::runtime_error("Unknown mode " + data.get(attrs.a_mode).value());
 
-	data.set(a_out, possumwood::opencv::Frame(out));
+	data.set(attrs.a_out, possumwood::opencv::Frame(out));
 
 	return dependency_graph::State();
 }
 
-void init(possumwood::Metadata& meta) {
+void MeanAttrs::init(possumwood::Metadata& meta) {
 	meta.addAttribute(a_in, "in", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
 	meta.addAttribute(a_superpixels, "superpixels", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
+	meta.addAttribute(a_mode, "mode", possumwood::Enum(s_mode.begin(), s_mode.end()));
 	meta.addAttribute(a_out, "out", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
 
 	meta.addInfluence(a_in, a_out);
 	meta.addInfluence(a_superpixels, a_out);
+	meta.addInfluence(a_mode, a_out);
 
-	meta.setCompute(compute);
+	meta.setCompute([this](dependency_graph::Values& data) {
+		return compute(data, *this);
+	});
 }
 
-possumwood::NodeImplementation s_impl("opencv/superpixels/mean", init);
+possumwood::NodeImplementation s_impl("opencv/superpixels/mean", [](possumwood::Metadata& meta) { s_meanAttrs.init(meta); });
 
 }
