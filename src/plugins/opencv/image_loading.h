65f94bf34d1b6a4d37ef5c10670e3314ca60e11d
Martin Prazak <martin.prazak@gmail.com>
2020-05-10 14:23:20 +0100

    Separated image loading into its own .h/.cpp

---
+++ src/plugins/opencv/image_loading.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <boost/filesystem.hpp>
+
+#include <opencv2/opencv.hpp>
+
+#include "exif.h"
+
+namespace possumwood { namespace opencv {
+
+std::pair<cv::Mat, Exif> load(const boost::filesystem::path& filename);
+
+} }
