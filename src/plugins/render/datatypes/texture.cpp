a9e0f7211c01783d0fddef7cf0ecf9373d181530
Martin Prazak <martin.prazak@gmail.com>
2020-05-31 09:42:37 +0100

    More generic OpenGL texture wrapper

--- src/plugins/render/datatypes/texture.cpp
+++ src/plugins/render/datatypes/texture.cpp
@@ -5,21 +5,39 @@
 
 namespace possumwood {
 
-Texture::Texture(const unsigned char* data, std::size_t width, std::size_t height, const Format& format) : m_id(0) {
+namespace {
+
+template<typename T>
+struct TextureTraits;
+
+template<>
+struct TextureTraits<const unsigned char*> {
+	static constexpr GLint GLType() { return GL_UNSIGNED_BYTE; };
+};
+
+template<>
+struct TextureTraits<const float*> {
+	static constexpr GLint GLType() { return GL_FLOAT; };
+};
+
+template<typename T_PTR>
+std::string makeTexture(GLuint id, T_PTR data, std::size_t width, std::size_t height, const Texture::Format& format) {
+	std::string error;
+
 	int original_alignment;
 	glGetIntegerv(GL_UNPACK_ALIGNMENT, &original_alignment);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, format.row_byte_align);
 
-	glGenTextures(1, &m_id);
-
-	glBindTexture(GL_TEXTURE_2D, m_id);
+	glBindTexture(GL_TEXTURE_2D, id);
 
 	// this assumes all image data are stored as a flat 8-bit per channel array!
 	switch(format.channel_order) {
-		case kRGB: glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data); break;
-		case kBGR: glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data); break;
-		case kGray: glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, data); break;
-		default: assert(false && "Invalid channel order");
+		case Texture::kRGB: glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, TextureTraits<T_PTR>::GLType(), data); break;
+		case Texture::kBGR: glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, TextureTraits<T_PTR>::GLType(), data); break;
+		case Texture::kGray: glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, width, height, 0, GL_RED, TextureTraits<T_PTR>::GLType(), data); break;
+		default:
+			assert(false && "Unsupported channel order");
+			error = "Unsupported channel order for OpenGL texture storage.";
 	}
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
@@ -30,41 +48,30 @@ Texture::Texture(const unsigned char* data, std::size_t width, std::size_t heigh
 	glBindTexture(GL_TEXTURE_2D, 0);
 
 	glPixelStorei(GL_UNPACK_ALIGNMENT, original_alignment);
+
+	return error;
+}
+
+}
+
+Texture::Texture(const unsigned char* data, std::size_t width, std::size_t height, const Format& format) : m_id(0) {
+	glGenTextures(1, &m_id);
+	const std::string err = makeTexture(m_id, data, width, height, format);
+
+	if(!err.empty()) {
+		glDeleteTextures(1, &m_id);
+		throw std::runtime_error(err);
+	}
 }
 
 Texture::Texture(const float* data, std::size_t width, std::size_t height, const Format& format) : m_id(0) {
-	int original_alignment;
-	glGetIntegerv(GL_UNPACK_ALIGNMENT, &original_alignment);
-	glPixelStorei(GL_UNPACK_ALIGNMENT, format.row_byte_align);
-
 	glGenTextures(1, &m_id);
+	const std::string err = makeTexture(m_id, data, width, height, format);
 
-	glBindTexture(GL_TEXTURE_2D, m_id);
-
-	// this assumes all image data are stored as a flat float per channel array!
-	switch(format.channel_order) {
-		case kRGB: glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, width, height, 0, GL_RGB, GL_FLOAT, data); break;
-		case kBGR: glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, width, height, 0, GL_BGR, GL_FLOAT, data); break;
-		case kGray: throw std::runtime_error("Greyscale HDR images not supported."); break;
-		default: assert(false && "Invalid channel order");
+	if(!err.empty()) {
+		glDeleteTextures(1, &m_id);
+		throw std::runtime_error(err);
 	}
-
-
-	if(format.channel_order == kRGB)
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, width, height, 0, GL_RGB, GL_FLOAT, data);
-	else if(format.channel_order == kBGR)
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
-	else
-		assert(false && "Invalid channel order");
-
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, format.interpolation);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, format.interpolation);
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	glPixelStorei(GL_UNPACK_ALIGNMENT, original_alignment);
 }
 
 Texture::~Texture() {
