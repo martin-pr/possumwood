20a68fe4a494699d20b5b30c10fa571ad3ba78c8
Martin Prazak <martin.prazak@gmail.com>
2020-09-06 22:46:24 +0100

    Introduced Nef Polyhedron datatype

--- src/plugins/cgal/cgal.cpp
+++ src/plugins/cgal/cgal.cpp
@@ -1,1 +1,11 @@
 #include "cgal.h"
+
+namespace possumwood {
+
+std::ostream& operator<<(std::ostream& out, const CGALNefPolyhedron& nef) {
+	out << "nef polyhedron - " << (*nef).number_of_vertices() << " vertices, " << (*nef).number_of_facets() << " facets, " << (*nef).number_of_volumes() << " volumes";
+
+	return out;
+}
+
+}  // namespace possumwood
--- src/plugins/cgal/datatypes/meshes.h
+++ src/plugins/cgal/datatypes/meshes.h
@@ -15,4 +15,13 @@ struct Traits<Meshes> {
 	}
 };
 
+template <>
+struct Traits<CGALNefPolyhedron> {
+	// static IO<std::shared_ptr<const Mesh>> io;
+
+	static constexpr std::array<float, 3> colour() {
+		return std::array<float, 3>{{0.5, 1, 0}};
+	}
+};
+
 }  // namespace possumwood
