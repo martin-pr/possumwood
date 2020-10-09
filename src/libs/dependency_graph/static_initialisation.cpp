1d3f277e17cd5148133942df290f147c7c2e156c
Martin Prazak <martin.prazak@gmail.com>
2020-10-09 23:38:18 +0100

    Simplification of dependency graph static initialisation

--- src/libs/dependency_graph/static_initialisation.cpp
+++ src/libs/dependency_graph/static_initialisation.cpp
@@ -8,70 +8,35 @@
 namespace dependency_graph {
 
 namespace {
-StaticInitialisation::Pimpl* s_currentInstance = nullptr;
-}
 
-/// Private implementation - only one instance can exist at any point
-struct StaticInitialisation::Pimpl {
-	Pimpl() {
-		assert(s_currentInstance == nullptr);
-		s_currentInstance = this;
-	}
-
-	~Pimpl() {
-		assert(s_currentInstance == this);
-		s_currentInstance = nullptr;
-	}
-
-	Pimpl(const Pimpl&) = delete;
-	Pimpl& operator=(const Pimpl&) = delete;
-
-	std::map<std::string, std::weak_ptr<const std::function<Data()>>> factories;
-};
-
-namespace {
-/// static Pimpl instance, if the Static initialisation class is not used explicitly - automagically created
-std::unique_ptr<StaticInitialisation::Pimpl> s_pimpl;
+typedef std::map<std::string, std::weak_ptr<const std::function<Data()>>> Factories;
 
 /// returns a valid instance - either explicitly created, or implicit
-StaticInitialisation::Pimpl& instance() {
-	if(s_currentInstance != nullptr)
-		return *s_currentInstance;
+Factories& factories() {
+	static std::unique_ptr<Factories> s_factories;
+	if(s_factories == nullptr)
+		s_factories = std::unique_ptr<Factories>(new Factories());
 
-	s_pimpl = std::unique_ptr<StaticInitialisation::Pimpl>(new StaticInitialisation::Pimpl());
-	assert(s_pimpl.get() == s_currentInstance);
-
-	return *s_currentInstance;
+	return *s_factories;
 };
+
 }  // namespace
 
-StaticInitialisation::StaticInitialisation() {
-	if(s_pimpl.get() != nullptr)
-		m_pimpl = std::move(s_pimpl);
-	else
-		m_pimpl = std::unique_ptr<Pimpl>(new Pimpl());
-
-	assert(m_pimpl.get() == s_currentInstance);
-}
-
-StaticInitialisation::~StaticInitialisation() {
-}
-
 FactoryHandle StaticInitialisation::registerDataFactory(const std::string& type, std::function<Data()> fn) {
-	auto it = instance().factories.find(type);
-	if(it != instance().factories.end())
+	auto it = factories().find(type);
+	if(it != factories().end())
 		return FactoryHandle(it->second.lock());
 
 	std::shared_ptr<const std::function<Data()>> ptr(new std::function<Data()>(fn));
-	instance().factories.insert(std::make_pair(type, ptr));
+	factories().insert(std::make_pair(type, ptr));
 
 	return FactoryHandle(ptr);
 }
 
 Data StaticInitialisation::create(const std::string& type) {
-	auto it = instance().factories.find(type);
+	auto it = factories().find(type);
 
-	if(it == instance().factories.end()) {
+	if(it == factories().end()) {
 		std::stringstream err;
 		err << "Error instantiating type '" << type << "' - no registered factory found (plugin not loaded?)";
 
--- src/libs/dependency_graph/static_initialisation.h
+++ src/libs/dependency_graph/static_initialisation.h
@@ -9,26 +9,10 @@
 
 namespace dependency_graph {
 
-/// An explicit static data holder, to allow for explicit control over static data destruction order.
-/// This is not a well encapsulated class, but as it is quite internal and not part of the main API, I am leaving it
-/// like this for now.
 class StaticInitialisation {
   public:
-	StaticInitialisation();
-	~StaticInitialisation();
-
 	static FactoryHandle registerDataFactory(const std::string& type, std::function<Data()> fn);
-
 	static Data create(const std::string& type);
-
-	/// Private implementation structure
-	struct Pimpl;
-
-  private:
-	std::unique_ptr<Pimpl> m_pimpl;
-
-	StaticInitialisation(const StaticInitialisation&) = delete;
-	StaticInitialisation& operator=(const StaticInitialisation&) = delete;
 };
 
 }  // namespace dependency_graph
