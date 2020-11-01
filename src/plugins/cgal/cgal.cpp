edb2b14739872d86c2352a16a8342092d91ff3e0
Martin Prazak <martin.prazak@gmail.com>
2020-11-01 21:54:43 +0000

    Added global unique IDs to each face

--- src/plugins/cgal/cgal.cpp
+++ src/plugins/cgal/cgal.cpp
@@ -2,8 +2,14 @@
 
 namespace possumwood {
 
+unsigned long getNewFaceId() {
+	static unsigned long s_id = 0;
+	return s_id++;
+}
+
 std::ostream& operator<<(std::ostream& out, const CGALNefPolyhedron& nef) {
-	out << "nef polyhedron - " << (*nef).number_of_vertices() << " vertices, " << (*nef).number_of_facets() << " facets, " << (*nef).number_of_volumes() << " volumes";
+	out << "nef polyhedron - " << (*nef).number_of_vertices() << " vertices, " << (*nef).number_of_facets()
+	    << " facets, " << (*nef).number_of_volumes() << " volumes";
 
 	return out;
 }
