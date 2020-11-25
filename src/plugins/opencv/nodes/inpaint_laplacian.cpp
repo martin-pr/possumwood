319334c037f98e092478f037191048f3956cd721
Martin Prazak <martin.prazak@gmail.com>
2020-11-25 19:17:53 +0000

    All the laplacian inpainting functionality moved to laplacian_inpainting.h

--- src/plugins/opencv/nodes/inpaint_laplacian.cpp
+++ src/plugins/opencv/nodes/inpaint_laplacian.cpp
@@ -2,223 +2,14 @@
 
 #include <mutex>
 
-#define EIGEN_STACK_ALLOCATION_LIMIT 0
-#include <Eigen/Sparse>
-
 #include <opencv2/opencv.hpp>
 
-#include <tbb/parallel_for.h>
-#include <tbb/task_group.h>
-
 #include <actions/traits.h>
+
 #include "frame.h"
+#include "laplacian_inpainting.h"
 
 namespace {
-
-class Triplets;
-
-class Row {
-  public:
-	Row() = default;
-
-	void addValue(int64_t row, int64_t col, double value) {
-		if(value != 0.0)
-			m_values[(row << 32) + col] += value;
-	}
-
-  private:
-	Row(const Row&) = delete;
-	Row& operator=(const Row&) = delete;
-
-	std::map<int64_t, double> m_values;
-
-	friend class Triplets;
-};
-
-class Triplets {
-  public:
-	Triplets(int rows, int cols) : m_rowCount(0), m_rows(rows), m_cols(cols) {
-	}
-
-	void addRow(const Row& r) {
-		for(auto& v : r.m_values) {
-			int32_t row = v.first >> 32;
-			int32_t col = v.first & 0xffffffff;
-
-			assert(row < m_rows);
-			assert(col < m_cols);
-
-			m_triplets.push_back(Eigen::Triplet<double>(m_rowCount, row * m_cols + col, v.second));
-		}
-
-		++m_rowCount;
-	}
-
-	std::size_t rows() const {
-		return m_rowCount;
-	}
-
-	const std::vector<Eigen::Triplet<double>>& triplets() const {
-		return m_triplets;
-	}
-
-  private:
-	std::vector<Eigen::Triplet<double>> m_triplets;
-
-	int m_rowCount, m_rows, m_cols;
-
-	friend class Row;
-};
-
-static const cv::Mat kernel = (cv::Mat_<double>(3, 3) << 0.0, -1.0, 0.0, -1.0, 4.0, -1.0, 0.0, -1.0, 0.0);
-
-// static const cv::Mat kernel = (cv::Mat_<double>(3,3) <<
-// 	-1.0, -1.0, -1.0,
-// 	-1.0,  8.0, -1.0,
-// 	-1.0, -1.0, -1.0
-// );
-
-// static const cv::Mat kernel = (cv::Mat_<double>(3,3) <<
-// 	-1.0, -2.0, -1.0,
-// 	-2.0, 12.0, -2.0,
-// 	-1.0, -2.0, -1.0
-// );
-
-// static const cv::Mat kernel = (cv::Mat_<double>(5,5) <<
-// 	 0.0,  0.0,  1.0,  0.0,  0.0,
-// 	 0.0,  2.0, -8.0,  2.0,  0.0,
-// 	 1.0, -8.0, 20.0, -8.0,  1.0,
-// 	 0.0,  2.0, -8.0,  2.0,  0.0,
-// 	 0.0,  0.0,  1.0,  0.0,  0.0
-// );
-
-float buildMatrices(const cv::Mat& image, const cv::Mat& mask, Eigen::SparseMatrix<double>& A, Eigen::VectorXd& b) {
-	Triplets triplets(image.rows, image.cols);
-	std::vector<double> values;
-
-	std::size_t validCtr = 0, interpolatedCtr = 0;
-
-	for(int y = 0; y < image.rows; ++y)
-		for(int x = 0; x < image.cols; ++x) {
-			Row row;
-
-			// masked and/or edge
-			if(mask.at<unsigned char>(y, x) > 128) {
-				values.push_back(0.0f);
-
-				// convolution
-				for(int yi = 0; yi < kernel.rows; ++yi)
-					for(int xi = 0; xi < kernel.cols; ++xi) {
-						int ypos = y + yi - kernel.rows / 2;
-						int xpos = x + xi - kernel.cols / 2;
-
-						// handling of edges - "clip" (or "mirror", commented out for now)
-						if(ypos < 0)
-							// ypos = -ypos;
-							ypos = 0;
-						if(ypos >= image.rows)
-							// ypos = (image.rows-1) - (ypos-image.rows);
-							ypos = image.rows - 1;
-
-						if(xpos < 0)
-							// xpos = -xpos;
-							xpos = 0;
-						if(xpos >= image.cols)
-							// xpos = (image.cols-1) - (xpos-image.cols);
-							xpos = image.cols - 1;
-
-						row.addValue(ypos, xpos, kernel.at<double>(yi, xi));
-					}
-
-				++interpolatedCtr;
-			}
-
-			// non-masked
-			if(mask.at<unsigned char>(y, x) <= 128) {
-				values.push_back(image.at<float>(y, x));
-				row.addValue(y, x, 1);
-
-				++validCtr;
-			}
-
-			triplets.addRow(row);
-		}
-
-	// initialise the sparse matrix
-	A = Eigen::SparseMatrix<double>(triplets.rows(), image.rows * image.cols);
-	A.setFromTriplets(triplets.triplets().begin(), triplets.triplets().end());
-
-	// and the "b" vector
-	assert(values.size() == triplets.rows());
-	b = Eigen::VectorXd(values.size());
-	for(std::size_t i = 0; i < values.size(); ++i)
-		b[i] = values[i];
-
-	return (float)validCtr / ((float)validCtr + (float)interpolatedCtr);
-}
-
-dependency_graph::State solve(const cv::Mat& input, const cv::Mat& mask, std::vector<float>& output) {
-	assert(input.rows == mask.rows);
-	assert(input.cols == mask.cols);
-	assert(input.type() == CV_32FC1);
-	assert(mask.type() == CV_8UC1);
-
-	output = std::vector<float>(input.rows * input.cols, 0.0f);
-
-	Eigen::SparseMatrix<double> A;
-	Eigen::VectorXd b, tmp;
-
-	dependency_graph::State state;
-
-	const float ratio = buildMatrices(input, mask, A, b);
-
-	if(ratio > 0.003) {
-		const char* stage = "solver construction";
-
-		Eigen::SparseLU<Eigen::SparseMatrix<double> /*, Eigen::NaturalOrdering<int>*/> chol(A);
-
-		if(chol.info() == Eigen::Success) {
-			stage = "analyze pattern";
-
-			chol.analyzePattern(A);
-
-			if(chol.info() == Eigen::Success) {
-				stage = "factorize";
-
-				chol.factorize(A);
-
-				if(chol.info() == Eigen::Success) {
-					stage = "solve";
-
-					tmp = chol.solve(b);
-
-					assert(tmp.size() == input.rows * input.cols);
-					for(int i = 0; i < tmp.size(); ++i) {
-						const int row = i / input.cols;
-						const int col = i % input.cols;
-						const int index = row * mask.cols + col;
-
-						assert((std::size_t)index < output.size());
-						output[index] = tmp[i];
-					}
-				}
-			}
-		}
-
-		if(chol.info() == Eigen::NumericalIssue)
-			state.addWarning("Decomposition failed - Eigen::NumericalIssue at stage " + std::string(stage));
-		else if(chol.info() == Eigen::NoConvergence)
-			state.addWarning("Decomposition failed - Eigen::NoConvergence at stage " + std::string(stage));
-		else if(chol.info() == Eigen::InvalidInput)
-			state.addWarning("Decomposition failed - Eigen::InvalidInput at stage " + std::string(stage));
-		else if(chol.info() != Eigen::Success)
-			state.addWarning("Decomposition failed - unknown error at stage " + std::string(stage));
-	}
-	else
-		state.addError("Solve failed - not enough non-masked data in input matrix");
-
-	return state;
-}
 
 dependency_graph::InAttr<possumwood::opencv::Frame> a_inFrame, a_inMask;
 dependency_graph::OutAttr<possumwood::opencv::Frame> a_outFrame;
@@ -226,51 +17,18 @@ dependency_graph::OutAttr<possumwood::opencv::Frame> a_outFrame;
 dependency_graph::State compute(dependency_graph::Values& data) {
 	dependency_graph::State state;
 
-	const cv::Mat& input = *data.get(a_inFrame);
-	const cv::Mat& mask = *data.get(a_inMask);
+	std::vector<cv::Mat> inputs;
+	inputs.push_back(*data.get(a_inFrame));
 
-	if(input.depth() != CV_32F)
-		throw std::runtime_error("Laplacian inpainting - input image type has to be CV_32F.");
-	if(mask.type() != CV_8UC1 && mask.type() != CV_8UC3)
-		throw std::runtime_error("Laplacian inpainting - mask image type has to be CV_8UC1 or CV_8UC3.");
-	if(input.empty() || mask.empty())
-		throw std::runtime_error("Laplacian inpainting - empty input image and/or mask.");
-	if(input.size != mask.size)
-		throw std::runtime_error("Laplacian inpainting - input and mask image size have to match.");
+	std::vector<cv::Mat> masks;
+	masks.push_back(*data.get(a_inMask));
 
-	std::vector<std::vector<float>> x(input.channels(), std::vector<float>(input.rows * input.cols, 0.0f));
+	std::vector<cv::Mat> result;
 
-	tbb::task_group tasks;
-	std::mutex state_mutex;
+	state.append(possumwood::opencv::inpaint(inputs, masks, result));
+	assert(result.size() == 1);
 
-	// split the inputs and masks per channel
-	std::vector<cv::Mat> inputs, masks;
-	cv::split(input, inputs);
-	cv::split(mask, masks);
-
-	assert((int)inputs.size() == input.channels());
-	assert((int)masks.size() == mask.channels());
-
-	for(int channel = 0; channel < input.channels(); ++channel) {
-		tasks.run([channel, &inputs, &masks, &x, &state, &state_mutex]() {
-			const dependency_graph::State currentState =
-			    solve(inputs[channel], masks.size() == 1 ? masks[0] : masks[channel], x[channel]);
-
-			std::lock_guard<std::mutex> guard(state_mutex);
-			state.append(currentState);
-		});
-	}
-
-	tasks.wait();
-
-	cv::Mat result = cv::Mat::zeros(input.rows, input.cols, input.type());
-	tbb::parallel_for(0, result.rows, [&](int yi) {
-		for(int xi = 0; xi < result.cols; ++xi)
-			for(int c = 0; c < input.channels(); ++c)
-				result.ptr<float>(yi, xi)[c] = x[c][yi * result.cols + xi];
-	});
-
-	data.set(a_outFrame, possumwood::opencv::Frame(result));
+	data.set(a_outFrame, possumwood::opencv::Frame(result[0]));
 
 	return state;
 }
