a58a3aae503f6c9362c34672beef3fae1a2e60da
Martin Prazak <martin.prazak@gmail.com>
2021-01-02 18:08:50 +0100

    Gaussian blur - enum implemented through a list of values

--- src/plugins/opencv/nodes/filter/gaussian_blur.cpp
+++ src/plugins/opencv/nodes/filter/gaussian_blur.cpp
@@ -13,33 +13,20 @@ dependency_graph::InAttr<float> a_sigmaX, a_sigmaY;
 dependency_graph::InAttr<possumwood::Enum> a_borderType;
 dependency_graph::OutAttr<possumwood::opencv::Frame> a_outFrame;
 
-int borderToEnum(const std::string& type) {
-	if(type == "BORDER_DEFAULT")
-		return cv::BORDER_DEFAULT;
-	else if(type == "BORDER_CONSTANT")
-		return cv::BORDER_CONSTANT;
-	else if(type == "BORDER_REPLICATE")
-		return cv::BORDER_REPLICATE;
-	else if(type == "BORDER_REFLECT")
-		return cv::BORDER_REFLECT;
-	else if(type == "BORDER_WRAP")
-		return cv::BORDER_WRAP;
-	else if(type == "BORDER_REFLECT_101")
-		return cv::BORDER_REFLECT_101;
-	else if(type == "BORDER_TRANSPARENT")
-		return cv::BORDER_TRANSPARENT;
-	else if(type == "BORDER_REFLECT101")
-		return cv::BORDER_REFLECT101;
-	else if(type == "BORDER_ISOLATED")
-		return cv::BORDER_ISOLATED;
-
-	throw std::runtime_error("Unknown border type " + type);
-}
+static std::vector<std::pair<std::string, int>> s_borderType = {{"BORDER_DEFAULT", cv::BORDER_DEFAULT},
+                                                                {"BORDER_CONSTANT", cv::BORDER_CONSTANT},
+                                                                {"BORDER_REPLICATE", cv::BORDER_REPLICATE},
+                                                                {"BORDER_REFLECT", cv::BORDER_REFLECT},
+                                                                {"BORDER_WRAP", cv::BORDER_WRAP},
+                                                                {"BORDER_REFLECT_101", cv::BORDER_REFLECT_101},
+                                                                {"BORDER_TRANSPARENT", cv::BORDER_TRANSPARENT},
+                                                                {"BORDER_REFLECT101", cv::BORDER_REFLECT101},
+                                                                {"BORDER_ISOLATED", cv::BORDER_ISOLATED}};
 
 dependency_graph::State compute(dependency_graph::Values& data) {
 	cv::Mat result = (*data.get(a_inFrame)).clone();
 
-	int border = borderToEnum(data.get(a_borderType).value());
+	int border = data.get(a_borderType).intValue();
 
 	cv::GaussianBlur(*data.get(a_inFrame), result, cv::Size(), data.get(a_sigmaX), data.get(a_sigmaY), border);
 
@@ -52,10 +39,7 @@ void init(possumwood::Metadata& meta) {
 	meta.addAttribute(a_inFrame, "in_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
 	meta.addAttribute(a_sigmaX, "sigma_x", 7.0f);
 	meta.addAttribute(a_sigmaY, "sigma_y", 0.0f);
-	meta.addAttribute(
-	    a_borderType, "border",
-	    possumwood::Enum({"BORDER_DEFAULT", "BORDER_CONSTANT", "BORDER_REPLICATE", "BORDER_REFLECT", "BORDER_WRAP",
-	                      "BORDER_REFLECT_101", "BORDER_TRANSPARENT", "BORDER_REFLECT101", "BORDER_ISOLATED"}));
+	meta.addAttribute(a_borderType, "border", possumwood::Enum(s_borderType.begin(), s_borderType.end()));
 	meta.addAttribute(a_outFrame, "out_frame", possumwood::opencv::Frame(), possumwood::AttrFlags::kVertical);
 
 	meta.addInfluence(a_inFrame, a_outFrame);
